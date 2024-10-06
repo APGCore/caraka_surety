301ed4a95d9f50946273ae4c44f7e70e8ef793b8
Arief Rachman Hakim <arief.rachman@confiction.com>
2024-10-06 15:35:23 +0700

    feat: products

--- app/Http/Controllers/Products/ProductController.php
+++ app/Http/Controllers/Products/ProductController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Products;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Product\ProductResource;
-use App\Http\Resources\Product\ProductResource;
 use App\Models\Product;
 use App\Models\ProductType;
 use Illuminate\Http\Request;
@@ -19,8 +18,8 @@ class ProductController extends Controller
     public function index(Request $request)
     {
         //
-        $component = $request->path().'/index';
-        $component = $request->path().'/index';
+        $component = $request->path() . '/index';
+        $component = $request->path() . '/index';
 
         $products = Product::search($request->get('search'))
             ->paginate(perPage: $request->perpage ?? 10)
@@ -33,8 +32,8 @@ class ProductController extends Controller
             'page_settings' => [
                 'title' => 'Produk',
             ],
-            'products' => fn () => $productResource,
-            'products' => fn () => $productResource,
+            'products' => fn() => $productResource,
+            'products' => fn() => $productResource,
         ]);
     }
 
@@ -44,8 +43,8 @@ class ProductController extends Controller
     public function create(Request $request)
     {
         //
-        $component = $request->path().'/index';
-        $component = $request->path().'/index';
+        $component = $request->path() . '/index';
+        $component = $request->path() . '/index';
 
         return inertia($component, [
             'page_settings' => [
@@ -95,13 +94,13 @@ class ProductController extends Controller
             }
 
             flashMessage('Produk Ditambahkan', 'Produk berhasil ditambahkan');
-            Log::info('Product Store: '.json_encode($product, JSON_PRETTY_PRINT));
-            Log::info('Product Store: '.json_encode($product, JSON_PRETTY_PRINT));
+            Log::info('Product Store: ' . json_encode($product, JSON_PRETTY_PRINT));
+            Log::info('Product Store: ' . json_encode($product, JSON_PRETTY_PRINT));
             DB::commit();
         } catch (\Throwable $th) {
             flashMessage('Gagal Menambahkan Produk', 'Terjadi kesalahan saat menambahkan produk', 'error');
-            Log::error('Produk Store: '.json_encode($th->getMessage(), JSON_PRETTY_PRINT));
-            Log::error('Produk Store: '.json_encode($th->getMessage(), JSON_PRETTY_PRINT));
+            Log::error('Produk Store: ' . json_encode($th->getMessage(), JSON_PRETTY_PRINT));
+            Log::error('Produk Store: ' . json_encode($th->getMessage(), JSON_PRETTY_PRINT));
             DB::rollBack();
         } finally {
             return redirect()->route('products.index');
@@ -130,14 +129,10 @@ class ProductController extends Controller
             'page_settings' => [
                 'title' => 'Edit Produk',
             ],
-<<<<<<< HEAD
+
             "product" => $product,
             // Include the product type in the response
             'product_types' => $product->productType ?? null, // Adjust 'product_type' to match the actual attribute
-=======
-            'product' => $product,
-
->>>>>>> 18cd421c2ba597567d81ffcb3074b58e3ed24f95
         ]);
     }
 
@@ -185,13 +180,13 @@ class ProductController extends Controller
 
             DB::commit();
             flashMessage('Produk Diperbarui', 'Produk berhasil diperbarui');
-            Log::info('Produk Update: '.json_encode($product, JSON_PRETTY_PRINT));
-            Log::info('Produk Update: '.json_encode($product, JSON_PRETTY_PRINT));
+            Log::info('Produk Update: ' . json_encode($product, JSON_PRETTY_PRINT));
+            Log::info('Produk Update: ' . json_encode($product, JSON_PRETTY_PRINT));
         } catch (\Throwable $th) {
             DB::rollBack();
             flashMessage('Gagal Memperbarui Produk', 'Terjadi kesalahan saat memperbarui Produk', 'error');
-            Log::error('Produk Update: '.json_encode($th->getMessage(), JSON_PRETTY_PRINT));
-            Log::error('Produk Update: '.json_encode($th->getMessage(), JSON_PRETTY_PRINT));
+            Log::error('Produk Update: ' . json_encode($th->getMessage(), JSON_PRETTY_PRINT));
+            Log::error('Produk Update: ' . json_encode($th->getMessage(), JSON_PRETTY_PRINT));
         } finally {
             return redirect()->route('products.index');
         }
@@ -209,13 +204,13 @@ class ProductController extends Controller
 
             DB::commit();
             flashMessage('Produk Dihapus', 'Produk berhasil dihapus');
-            Log::info('Produk Delete: '.json_encode($product, JSON_PRETTY_PRINT));
-            Log::info('Produk Delete: '.json_encode($product, JSON_PRETTY_PRINT));
+            Log::info('Produk Delete: ' . json_encode($product, JSON_PRETTY_PRINT));
+            Log::info('Produk Delete: ' . json_encode($product, JSON_PRETTY_PRINT));
         } catch (\Throwable $th) {
             DB::rollBack();
             flashMessage('Gagal Menghapus Produk', 'Terjadi kesalahan saat menghapus Produk', 'error');
-            Log::error('Produk Delete: '.json_encode($th->getMessage(), JSON_PRETTY_PRINT));
-            Log::error('Produk Delete: '.json_encode($th->getMessage(), JSON_PRETTY_PRINT));
+            Log::error('Produk Delete: ' . json_encode($th->getMessage(), JSON_PRETTY_PRINT));
+            Log::error('Produk Delete: ' . json_encode($th->getMessage(), JSON_PRETTY_PRINT));
         } finally {
             return redirect()->back();
         }
