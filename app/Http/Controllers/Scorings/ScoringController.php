1213ed2b4cb69a3487c56a40575968f8b588ef8e
Arief Rachman Hakim <arief.rachman@confiction.com>
2024-10-18 00:08:13 +0700

    merge

--- app/Http/Controllers/Scorings/ScoringController.php
+++ app/Http/Controllers/Scorings/ScoringController.php
@@ -134,20 +134,18 @@ class ScoringController extends Controller
                 $scoring->update($request->only('name', 'min_point'));
 
                 DB::commit();
-                // flashMessage('Skoring Diperbarui', 'Skoring berhasil diperbarui');
 
-                // return redirect()->route('scoring.index');
                 return $this->responseSuccess('Skoring berhasil diedit!');
             } else {
                 throw new ThrottleRequestsException('Skoring tidak ditemukan');
             }
         } catch (\Throwable $th) {
-            // flashMessage('Gagal Mengubah Skoring', 'Terjadi kesalahan saat merubah skoring', 'error');
+
             Log::error('Scoring Update: ' . json_encode($th->getMessage(), JSON_PRETTY_PRINT));
 
             DB::rollBack();
 
-            // return redirect()->back()->with('error', $th->getMessage());
+
 
             return $this->responseSuccess('Skoring gagal diedit');
         }
