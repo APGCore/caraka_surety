a3d91107640c79175340e5245b53ae81bef743f0
Wanda Aizul Fahmi <wandaaizulf@gmail.com>
2024-10-22 23:17:06 +0700

    feat : update template permohonan

--- resources/js/pages/admin/submission/template-surat-permohonan.js
+++ resources/js/pages/admin/submission/template-surat-permohonan.js
@@ -1,90 +1,97 @@
 const secondTemplateContent = `
-<h2 style="text-align: center;">SURAT PERMOHONAN (DUMMY)</h2>
+<h2 style="text-align: center;">SURAT PERMOHONAN JAMINAN</h2>
 
-<p style="text-align: justify; margin-bottom: 10px;">Nomor Surat  :  40.91.02.AAAAAAA.11.22</p>
-<p style="text-align: justify; margin-bottom: 10px;">Perihal      :  Contoh Surat Permohonan</p>
+<div style="width: 100%; overflow: hidden;">
+    <div style="float: left; width: 50%;">
+        <p style="margin-bottom: 10px;">Perihal: Permohonan Jaminan</p>
+    </div>
+    <div style="float: right; width: 50%; text-align: right;">
+        <p style="margin-bottom: 10px;">Jakarta, 28 Agustus 2024</p>
+        <p style="margin-bottom: 10px;">Kepada Yth.<br>PT. (Di Isi Sesuai Dengan Nama Bank Penerbit)<br>di Tempat</p>
+    </div>
+</div>
 
+<p style="text-align: justify; margin-bottom: 10px;">Yang Bertandatangan di bawah ini:</p>
 <table style="width: 100%; border-collapse: collapse; line-height: 1.5;">
     <tr>
-        <td style="width: 30px; vertical-align: top;">1.</td>
-        <td style="text-align: justify;">Dengan ini dinyatakan, bahwa kami: PT. MOTI BATARA ALKESINDO (NAMA PRINCIPAL),
-        Jl. Agave Raya Blok A1 No. 19A RT. 09 RW. 04 Kedoya Selatan, Kebon Jeruk, Jakarta Barat (ALAMAT PRINCIPAL)
-        sebagai Penyedia, selanjutnya disebut TERJAMIN, dan PT. ASURANSI UMUM VIDEI,
-        Graha Mustika Ratu, Lantai 1, Jl. Jend. Gatot Subroto Kav. 74-75, Jakarta 12870 sebagai Asuransi,
-        selanjutnya disebut sebagai PENJAMIN, bertanggung jawab dan dengan tegas terikat pada Pejabat Penandatangan Kontrak
-        Politeknik Kesehatan Jakarta III (NAMA OBLIGEE), Jl. Arteri JORR, Jatiwarna, Kec. Pondok Melati, Bekasi (ALAMAT OBLIGEE)
-        sebagai Pemilik Pekerjaan, selanjutnya disebut PENERIMA JAMINAN atas uang sejumlah Rp. 32.077.650,-
-        (terbilang: Tiga Puluh Dua Juta Tujuh Puluh Tujuh Ribu Enam Ratus Lima Puluh Rupiah) (BESARAN NILAI JAMINAN).</td>
+        <td style="width: 200px; vertical-align: top;">Nama Perusahaan</td>
+        <td>: PT. MOTI BATARA ALKESINDO</td>
     </tr>
     <tr>
-        <td style="vertical-align: top;">2.</td>
-        <td style="text-align: justify;">Maka kami, TERJAMIN dan PENJAMIN dengan ini mengikatkan diri untuk melakukan pembayaran jumlah tersebut di atas
-        dengan baik dan benar bilamana TERJAMIN tidak memenuhi kewajiban dalam melaksanakan pelelangan Pekerjaan
-        Pengadaan Alat Laboratorium Jurusan Keperawatan Poltekkes Kemenkes Jakarta III (NAMA PEKERJAAN)
-        yang telah dipercayakan kepadanya atas dasar Surat Penunjukan Penyedia Barang/Jasa (SPPBJ) (NAMA DOKUMEN)
-        Nomor: KN.01.01/V/6747/2019 (NOMOR DOKUMEN) tanggal 30 September 2019 (TANGGAL DOKUMEN).</td>
+        <td style="vertical-align: top;">Alamat Kantor</td>
+        <td>: Jl. Agave Raya Blok A1 No. 19A, RT. 09 RW. 04, Kedoya Selatan, Kebon Jeruk, Jakarta Barat</td>
     </tr>
     <tr>
-        <td style="vertical-align: top;">3.</td>
-        <td style="text-align: justify;">Surat Jaminan ini berlaku selama 90 (Sembilan Puluh) hari kalender dan efektif mulai dari 01 Oktober 2019
-        sampai dengan tanggal 29 Desember 2019 (JANGKA WAKTU).</td>
+        <td style="vertical-align: top;">NPWP Perusahaan</td>
+        <td>: 01.234.567.8-999.000</td>
     </tr>
     <tr>
-        <td style="vertical-align: top;">4.</td>
-        <td style="text-align: justify;">Jaminan ini berlaku apabila:</td>
+        <td style="vertical-align: top;">Nama Penanggung Jawab</td>
+        <td>: Muhammad Ridzqan Hanafis</td>
     </tr>
     <tr>
-        <td style="vertical-align: top;"></td>
-        <td>
-            <ul style="text-align: justify; line-height: 1.5;">
-                <li>TERJAMIN tidak menyelesaikan pekerjaan tersebut pada waktunya dengan baik dan benar sesuai dengan ketentuan dalam Kontrak;</li>
-                <li>Pemutusan kontrak akibat kesalahan TERJAMIN.</li>
-            </ul>
-        </td>
-    </tr>
-    <tr>
-        <td style="vertical-align: top;">5.</td>
-        <td style="text-align: justify;">PENJAMIN akan membayar kepada PENERIMA JAMINAN sejumlah nilai jaminan tersebut di atas dalam waktu paling lambat
-        14 (empat belas) hari kerja tanpa syarat setelah menerima tuntutan pencairan secara tertulis dari PENERIMA JAMINAN
-        berdasar Keputusan PENERIMA JAMINAN mengenai pengenaan sanksi akibat TERJAMIN cidera janji.</td>
-    </tr>
-    <tr>
-        <td style="vertical-align: top;">6.</td>
-        <td style="text-align: justify;">Menunjuk pada Pasal 1832 KUH Perdata dengan ini ditegaskan kembali bahwa PENJAMIN melepaskan hak-hak istimewa
-        untuk menuntut supaya harta benda TERJAMIN lebih dahulu disita dan dijual guna dapat melunasi hutangnya
-        sebagaimana dimaksud dalam Pasal 1831 KUH Perdata.</td>
-    </tr>
-    <tr>
-        <td style="vertical-align: top;">7.</td>
-        <td style="text-align: justify;">Tuntutan Pencairan terhadap PENJAMIN berdasarkan Jaminan ini harus sudah diajukan selambat-lambatnya
-        dalam waktu 30 (tiga puluh) hari kalender sesudah berakhirnya masa berlaku Jaminan ini.</td>
+        <td style="vertical-align: top;">Jabatan Penanggung Jawab</td>
+        <td>: Direktur</td>
     </tr>
 </table>
 
-<table style="width: 100%; border-collapse: collapse; margin-top: 20px;">
+<p style="text-align: justify; margin-bottom: 10px;">Dengan ini mengajukan permohonan Kontra Bank Garansi, dengan ketentuan sebagai berikut:</p>
+<table style="width: 100%; border-collapse: collapse; line-height: 1.5;">
     <tr>
-        <td style="text-align: left;">Dikeluarkan di Jakarta pada tanggal 01 Oktober 2019 (TANGGAL PENERBITAN)</td>
-        <td></td> <!-- Kolom kosong untuk menjaga keselarasan -->
+        <td style="width: 200px; vertical-align: top;">Jenis Jaminan</td>
+        <td>: Jaminan Pelaksanaan</td>
     </tr>
     <tr>
-        <td style="text-align: left;">PT. ASURANSI UMUM VIDEI (TERJAMIN) (PENJAMIN)</td>
-        <td style="text-align: right;">PT. MOTI BATARA ALKESINDO (NAMA PRINCIPAL)</td>
+        <td style="vertical-align: top;">Penerbit Bank Garansi</td>
+        <td>: PT. ASURANSI UMUM VIDEI</td>
     </tr>
     <tr>
-        <td style="text-align: left;"><br></td>
-        <td></td>
+        <td style="vertical-align: top;">Atas Kepentingan</td>
+        <td>: Politeknik Kesehatan Jakarta III</td>
     </tr>
     <tr>
-        <td style="text-align: left;">
-            Muhammad Ridzqan Hanafis (NAMA PENANGGUNG JAWAB)<br>
-            D i r e k t u r (JABATAN PJ)
-        </td>
-        <td style="text-align: right;">
-            Marthen F Surentu<br>
-            Kepala Cabang
-        </td>
+        <td style="vertical-align: top;">Alamat Pemilik Proyek</td>
+        <td>: Jl. Arteri JORR, Jatiwarna, Pondok Melati, Bekasi</td>
+    </tr>
+    <tr>
+        <td style="vertical-align: top;">Nama Proyek</td>
+        <td>: Pengadaan Alat Laboratorium Jurusan Keperawatan</td>
+    </tr>
+    <tr>
+        <td style="vertical-align: top;">Nilai Jaminan</td>
+        <td>: Rp. 32.077.650,- (Tiga Puluh Dua Juta Tujuh Puluh Tujuh Ribu Enam Ratus Lima Puluh Rupiah)</td>
+    </tr>
+    <tr>
+        <td style="vertical-align: top;">Jangka Waktu</td>
+        <td>: 90 (Sembilan Puluh) hari kalender, dari 01 Oktober 2019 hingga 29 Desember 2019</td>
+    </tr>
+    <tr>
+        <td style="vertical-align: top;">Dasar Dokumen</td>
+        <td>: Surat Penunjukan Penyedia Barang/Jasa (SPPBJ) No. KN.01.01/V/6747/2019</td>
     </tr>
 </table>
+
+<p style="text-align: justify;">Demikian permohonan ini kami sampaikan, dan atas perhatiannya kami ucapkan terima kasih.</p>
+
+<div style="width: 100%; overflow: hidden;">
+    <table style="width: 100%; border-collapse: collapse;">
+        <tr>
+            <!-- Kolom kiri kosong -->
+            <td style="width: 50%;"></td>
+
+            <!-- Kolom kanan berisi teks -->
+            <td style="width: 50%; text-align: left;">
+                <p style="margin-bottom: 10px;">Nama Principal</p>
+                <p style="margin-bottom: 50px;"></p> <!-- Space for signature -->
+                <p style="margin-bottom: 10px;">Nama Penanggung Jawab</p>
+                <p style="margin-bottom: 10px;">Jabatan</p>
+            </td>
+        </tr>
+    </table>
+</div>
+
+
+
 `;
 
 export default secondTemplateContent;
