e70862af76419e0ca9e4fe820f046e93df6a772e
Wanda Aizul Fahmi <wandaaizulf@gmail.com>
2024-10-25 17:19:13 +0700

    feat: add surety bond jastan

--- resources/js/pages/admin/submission/detail/index.tsx
+++ resources/js/pages/admin/submission/detail/index.tsx
@@ -4,7 +4,7 @@ import AdminLayout from "@/layouts/admin";
 import templateDraftSurety from "@/pages/output_templates/template-draft-surety";
 import templateAnalyst from "@/pages/output_templates/template-hasil-analisa";
 import templateContent from "@/pages/output_templates/template-surat-pelaksanaan";
-import secondTemplateContent from "@/pages/output_templates/template-surat-permohonan-surety-bond-videi";
+import secondTemplateContent from "@/pages/output_templates/template-surat-permohonan-surety-bond-jastan";
 import { Head, Link, usePage } from "@inertiajs/react";
 import { useEffect } from "react";
 import { Editor } from "tinymce";
