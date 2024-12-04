2e9c5c9ed3a2c963e66c9d678d447f07038d5108
Iman Nurjaman <imannj69@gmail.com>
2024-12-04 22:22:02 +0700

    feat(main): update build view

--- public/build/assets/export-docs-datatable-AFTJWfRC.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-CD3kv2xt.js";import{c as s}from"./cn-CcFn7B-c.js";import{B as a}from"./button-hVYPyWKv.js";const c=({className:t,onClick:o})=>r.jsx(a,{className:s(t),onClick:o,children:"Export"});export{c as E};
--- public/build/assets/section-title-Df5U97EJ.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./app-CD3kv2xt.js";const i=()=>t.jsx("div",{children:"SectionTitle"});export{i as default};
