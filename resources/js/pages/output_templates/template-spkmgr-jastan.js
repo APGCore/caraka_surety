15a78fa8adc43ee8b3520fe8f8de7143dff18847
waaizulfahmi <wandaaizulf@gmail.com>
2025-01-07 00:06:46 +0700

    feat : update spkmgr

---
+++ resources/js/pages/output_templates/template-spkmgr-jastan.js
@@ -0,0 +1,118 @@
+const templateSkmgrJastan = `
+  <div style="max-width: 900px; margin: 20px auto; padding: 20px; border: 1px solid #000; border-radius: 8px; font-family: Arial, sans-serif; background-color: #fff;">
+    <h1 style="text-align: center; font-size: 18px; color: #000; margin-bottom: 20px; text-transform: uppercase; border: 1px solid #000; padding: 10px; display: inline-block;">
+      Perjanjian Ganti Rugi Kepada Surety
+    </h1>
+
+    <p style="font-size: 14px; line-height: 1.8; text-align: justify; color: #000;">
+      Dengan ini dinyatakan bahwa yang bertandatangan di bawah ini selanjutnya disebut sebagai
+      <strong>PRINCIPAL</strong> atau <strong>TERJAMIN</strong>, dengan ini berjanji akan membayar ganti rugi kepada
+      <strong>PT. Asuransi Jasa Tania, Tbk</strong>, yang selanjutnya disebut sebagai <strong>SURETY</strong>
+      dengan ketentuan sebagaimana tersebut di bawah ini:
+    </p>
+
+    <p style="font-size: 14px; line-height: 1.8; text-align: justify; color: #000;">
+      Berdasarkan hal-hal tersebut di atas, sehubungan dengan permohonan jaminan yang akan diterbitkan oleh
+      SURETY, maka PRINCIPAL atau TERJAMIN dan masing-masing dari mereka serta masing-masing ahli waris,
+      pelaksana, pengurus, penggantinya dan orang-orang yang ditunjuk oleh mereka, baik bersama-sama maupun
+      sendiri-sendiri, dengan ini mufakat dan setuju untuk membayarkan ganti rugi kepada SURETY dan membebaskannya
+      dari kerugian terhadap semua tindakan berupa tuntutan, tagihan, tuntutan biaya, kehilangan, atau biaya
+      apapun termasuk biaya penasihat hukum yang oleh SURETY, harus dibayarkan sebagai akibat dari telah
+      diberikannya jaminan tersebut, termasuk setiap gugatan untuk memaksakan pelaksanaan kewajiban-kewajiban
+      dari perjanjian ini dan setiap perubahan atau endorsement yang merupakan lampirannya.
+    </p>
+
+    <p style="font-size: 14px; line-height: 1.8; color: #000; text-align: justify;">
+      Selanjutnya telah dimengerti dan disetujui sebagai ketentuan dari Perjanjian Ganti Rugi ini:
+    </p>
+
+    <ol style="font-size: 14px; color: #000; line-height: 1.6; margin: 20px 0; padding-left: 40px;">
+      <li>
+        Bahwa yang bertandatangan di bawah ini, PRINCIPAL atau TERJAMIN dan masing-masing dari mereka serta masing-masing ahli waris, pelaksana, pengurus, penggantinya, dan orang-orang yang ditunjuk oleh mereka, atas permintaan SURETY akan:
+        <ol type="a" style="margin: 10px 0; padding-left: 20px;">
+          <li>Membayar dengan semestinya atau menyuruh membayar kepada SURETY, premi, pajak premi atau biaya-biaya lainnya sebagaimana ditentukan oleh SURETY untuk jaminan tersebut.</li>
+          <li>Membayar dengan semestinya atau menyuruh membayar kepada SURETY, semua jumlah uang, denda, atau beban lainnya yang mungkin timbul sebagai akibat dari penjaminan.</li>
+          <li>Menanggung semua tuntutan (klaim) terhadap SURETY.</li>
+        </ol>
+      </li>
+      <li>
+        Bahwa SURETY, atas kebijaksanaannya sendiri, dapat menyelesaikan atau menolak tuntutan dari pihak-pihak yang mengajukan, dapat menguasai, mengurus, mengelola, dan mengakhiri setiap klaim yang diajukan, dan pembayaran yang dibuat oleh SURETY dalam kaitannya dengan klaim tersebut akan menjadi beban dan tanggung jawab PRINCIPAL.
+      </li>
+      <li>
+        Bahwa segera setelah SURETY diminta untuk membayar jumlah jaminan atau uang akibat diterbitkannya surat jaminan, PRINCIPAL wajib mengganti pembayaran tersebut dalam waktu tujuh (7) hari.
+      </li>
+      <li>
+        Bahwa PRINCIPAL atau TERJAMIN dan masing-masing dari mereka serta ahli waris, pelaksana, pengurus, penggantinya, dan orang-orang yang ditunjuk oleh mereka, wajib memberikan jaminan tambahan sesuai kebutuhan SURETY.
+      </li>
+    </ol>
+
+    <p style="font-size: 14px; line-height: 1.8; text-align: justify; color: #000;">
+      Demikian perjanjian ini dibuat untuk dipatuhi oleh semua pihak yang terlibat tanpa terkecuali.
+    </p>
+
+    <div style="margin-top: 30px; font-size: 14px; color: #000;">
+      <p>
+        <strong>Ditandatangani di:</strong> [<br />
+        <strong>Tanggal:</strong> [DATE ]
+      </p>
+
+      <p>
+        <strong>Nama Perusahaan:</strong> [PRINCIPAL_NAME]<br />
+        <strong>Alamat Perusahaan:</strong> [PRINCIPAL_ADDRESS]
+      </p>
+    </div>
+  </div>
+
+   <div style="max-width: 800px; margin: 20px auto; padding: 20px; border: 1px solid #000; border-radius: 8px; font-family: Arial, sans-serif; background-color: #fff;">
+    <p style="font-size: 14px; line-height: 1.8; text-align: justify; color: #000;">
+      Sebagai bukti dari persetujuan di atas, maka <strong>PRINCIPAL</strong> atau <strong>TERJAMIN</strong> dengan ini membubuhkan tanda tangan dan cap perusahaan mereka pada Perjanjian Ganti Rugi ini.
+    </p>
+
+    <div style="margin-top: 20px; font-size: 14px; color: #000; line-height: 2;">
+      <p>
+        <strong>Di</strong>: [PRINCIPAL_ADDRESS]<br />
+        <strong>Hari</strong>: [DAY_NAME]<br />
+        <strong>Tanggal</strong>: [DATE]
+      </p>
+    </div>
+
+    <div style="margin-top: 20px; font-size: 14px; color: #000; line-height: 2;">
+      <p>
+        <strong>Sebagai PRINCIPAL atau TERJAMIN</strong><br /><br />
+        <strong>Nama Perusahaan</strong>: [PRINCIPAL_NAME]<br />
+        <strong>Alamat</strong>: [PRINCIPAL_ADDRESS]<br />
+        ..............................................................................................
+      </p>
+    </div>
+
+    <div style="margin-top: 20px; font-size: 14px; text-align: left;">
+      <div style="border: 1px dashed #000; display: inline-block; padding: 10px; text-align: center;">
+        <p style="font-size: 12px;">Materai</p>
+        <p style="font-size: 12px;">Rp 6.000,-</p>
+      </div>
+    </div>
+
+    <div style="margin-top: 40px; font-size: 14px; color: #000; line-height: 2;">
+      <p>
+        [DIRECTOR_NAME]<br />
+        <strong>Direksi</strong>
+      </p>
+    </div>
+
+    <div style="margin-top: 20px; font-size: 14px; color: #000; line-height: 2;">
+      <p>
+        <strong>Persetujuan pemegang saham</strong> (diwakili oleh Komisaris sesuai Akte perubahan terakhir)
+      </p>
+    </div>
+
+    <div style="margin-top: 40px; font-size: 14px; text-align: left;">
+      <p>
+        ttd ..................................................<br /><br />
+        -----------------------------------------<br />
+        <strong>Komisaris</strong>
+      </p>
+    </div>
+  </div>
+`;
+
+export default templateSkmgrJastan;
