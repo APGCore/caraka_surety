defe70471fe2f00111b0a0c22ace863596112183
Arief Rachman Hakim <arief.rachman@confiction.com>
2025-02-02 19:05:17 +0700

    fix: eroor kepala cabang not founfd

--- resources/js/layouts/staff-cabang/index.tsx
+++ resources/js/layouts/staff-cabang/index.tsx
@@ -1,29 +1,9 @@
 import useFlashMessageToast from "@/common/hooks/general/use-flash-message";
-import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/_shadcn-ui/collapsible";
 import { Separator } from "@/components/_shadcn-ui/separator";
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarGroup,
-  SidebarHeader,
-  SidebarInset,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-  SidebarMenuSub,
-  SidebarMenuSubButton,
-  SidebarMenuSubItem,
-  SidebarProvider,
-  SidebarRail,
-  SidebarTrigger,
-} from "@/components/_shadcn-ui/sidebar";
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/_shadcn-ui/sidebar";
 import { Toaster } from "@/components/_shadcn-ui/toaster";
 import Clock from "@/components/common/clock";
-import RenderList from "@/components/common/render-list";
-import { SidebarMenuCustom } from "@/components/sidebar/app-sidebar/sidebar-menu-custom";
-import { Link } from "@inertiajs/react";
-import { ChevronRight, GalleryVerticalEnd } from "lucide-react";
+import AppSidebar from "@/components/sidebar/app-sidebar";
 import { staffCabangRoute } from "./staff-cabang-layout.constant";
 import { StaffCabangLayoutPageProps } from "./staff-cabang-layout.type";
 
@@ -32,85 +12,9 @@ export const StaffCabangLayoutPage: StaffCabangLayoutPageProps = ({ children, us
 
   return (
     <SidebarProvider>
-      <Sidebar collapsible="icon">
-        <SidebarHeader>
-          <SidebarMenu>
-            <SidebarMenuItem>
-              <SidebarMenuButton
-                size="lg"
-                className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground">
-                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                  <GalleryVerticalEnd className="size-4" />
-                </div>
-                <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-semibold">APG - Core System</span>
-                  <span className="truncate text-xs">Role Staff Cabang</span>
-                </div>
-              </SidebarMenuButton>
-            </SidebarMenuItem>
-          </SidebarMenu>
-        </SidebarHeader>
-        <SidebarContent>
-          <SidebarGroup>
-            <SidebarMenu>
-              <RenderList
-                of={staffCabangRoute.navMain}
-                render={(item) => {
-                  const hasActiveSubItem = item?.items.some((subItem) => route().current(`${subItem.route_name}.*`));
-                  return (
-                    <Collapsible key={item.title} asChild defaultOpen={hasActiveSubItem} className="group/collapsible">
-                      {item.items.length > 0 ? (
-                        <SidebarMenuItem>
-                          <CollapsibleTrigger asChild>
-                            <SidebarMenuButton tooltip={item.title}>
-                              {item.icon && <item.icon />}
-                              <span>{item.title}</span>
-                              <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
-                            </SidebarMenuButton>
-                          </CollapsibleTrigger>
-                          <CollapsibleContent>
-                            <SidebarMenuSub>
-                              <RenderList
-                                of={item.items}
-                                render={(subItem) => (
-                                  <SidebarMenuSubItem key={subItem.title}>
-                                    <SidebarMenuSubButton asChild isActive={route().current(`${subItem.route_name}.*`)}>
-                                      <Link href={subItem.href}>
-                                        <span>{subItem.title}</span>
-                                      </Link>
-                                    </SidebarMenuSubButton>
-                                  </SidebarMenuSubItem>
-                                )}
-                              />
-                            </SidebarMenuSub>
-                          </CollapsibleContent>
-                        </SidebarMenuItem>
-                      ) : (
-                        <SidebarMenuItem>
-                          <SidebarMenuButton
-                            tooltip={item.title}
-                            isActive={item.route_name ? route().current(`${item.route_name}.*`) : false}>
-                            {item.icon && <item.icon />}
-                            <Link href={item?.href ?? "#"} className="w-full">
-                              <span>{item.title}</span>
-                            </Link>
-                          </SidebarMenuButton>
-                        </SidebarMenuItem>
-                      )}
-                    </Collapsible>
-                  );
-                }}
-              />
-            </SidebarMenu>
-          </SidebarGroup>
-        </SidebarContent>
-        <SidebarFooter>
-          <SidebarMenuCustom user={user} />
-        </SidebarFooter>
-        <SidebarRail />
-      </Sidebar>
+      <AppSidebar user={user} routes={staffCabangRoute} />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear  fixed z-20 top-0 group-has-[[data-collapsible=icon]]/sidebar-wrapper:w-[calc(100%_-_48px)] md:w-[calc(100%_-_255px)] w-[calc(100%_-_0px)] bg-white border-b-[1px]">
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear  fixed z-20 top-0 group-has-[[data-collapsible=icon]]/sidebar-wrapper:w-[calc(100%_-_48px)] md:w-[calc(100%_-_269px)] w-[calc(100%_-_0px)] bg-white border-b-[1px]">
           <div className="flex justify-between pr-4 w-full">
             <div className="flex items-center gap-2 px-4">
               <SidebarTrigger className="-ml-1" />
@@ -121,7 +25,9 @@ export const StaffCabangLayoutPage: StaffCabangLayoutPageProps = ({ children, us
             </div>
           </div>
         </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-[84px] pb-[50px]">{children}</div>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-[84px] pb-[50px]  xl:max-w-6xl w-full mx-auto">
+          {children}
+        </div>
       </SidebarInset>
       <Toaster />
     </SidebarProvider>
--- resources/js/layouts/staff-cabang/staff-cabang-layout.constant.ts
+++ resources/js/layouts/staff-cabang/staff-cabang-layout.constant.ts
@@ -1,40 +1,33 @@
+import { IAppSideBarMenu } from "@/components/sidebar/app-sidebar/app-sidebar.type";
 import { Archive, Home } from "lucide-react";
 
-export const staffCabangRoute = {
-  navMain: [
-    {
-      title: "Dashboard",
-      url: "#",
-      route_name: "staff-cabang",
-      href: route("staff-cabang.index"),
-      icon: Home,
-      isActive: true,
-      items: [],
-    },
-    {
-      title: "Kelola Pengajuan",
-      url: "#",
-      icon: Archive,
-      items: [
-        {
-          title: "Buat Pengajuan",
-          route_name: "staff-cabang-submission-create",
-          href: route("staff-cabang-submission-create.submission"),
-          url: "#",
-        },
-        {
-          title: "Riwayat Pengajuan",
-          route_name: "staff-cabang-submission-history",
-          href: route("staff-cabang-submission-history.submission"),
-          url: "#",
-        },
-        {
-          title: "Draft Dokumen Pengajuan",
-          route_name: "staff-cabang-submission-document-draft",
-          href: route("staff-cabang-submission-document-draft.submission"),
-          url: "#",
-        },
-      ],
-    },
-  ],
-};
+export const staffCabangRoute: IAppSideBarMenu[] = [
+  //   {
+  //     title: "Dashboard",
+  //     route_name: "staff-cabang",
+  //     href: route("staff-cabang.index"),
+  //     icon: Home,
+  //     items: [],
+  //   },
+  //   {
+  //     title: "Kelola Pengajuan",
+  //     icon: Archive,
+  //     items: [
+  //       {
+  //         title: "Buat Pengajuan",
+  //         route_name: "staff-cabang-submission-create",
+  //         href: route("staff-cabang-submission-create.submission"),
+  //       },
+  //       {
+  //         title: "Riwayat Pengajuan",
+  //         route_name: "staff-cabang-submission-history",
+  //         href: route("staff-cabang-submission-history.submission"),
+  //       },
+  //       {
+  //         title: "Draft Dokumen Pengajuan",
+  //         route_name: "staff-cabang-submission-document-draft",
+  //         href: route("staff-cabang-submission-document-draft.submission"),
+  //       },
+  //     ],
+  //   },
+];
