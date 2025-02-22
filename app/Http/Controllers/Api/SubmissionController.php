4ea80117d8b106a3d55d0f861a6e773598b34d2e
Iman Nurjaman <imannj69@gmail.com>
2025-02-22 23:22:16 +0700

    update: get submission

--- app/Http/Controllers/Api/SubmissionController.php
+++ app/Http/Controllers/Api/SubmissionController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Enums\JobGroup;
+use App\Enums\JobType;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\Submission\CallbackRequest;
+use App\Models\Product\Product;
+use App\Models\Product\ProductType;
+use App\Models\Submission\SourceOfFund;
 use App\Models\Submission\Submission;
 use App\Services\HostToHostService;
-use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class SubmissionController extends Controller
@@ -18,11 +22,11 @@ class SubmissionController extends Controller
         $this->hostToHostService = $hostToHostService;
     }
 
-    public function show(Request $request, Submission $submission): JsonResponse
+    public function show(Submission $submission): JsonResponse
     {
         $submission->load([
-            'principal:id,name,telephone,pic,npwp,nib,siup_siujk,head_name,business_fields,'.
-            'director_name,director_position,director_phone,commissioner,year_established,'.
+            'principal:id,name,telephone,pic,npwp,nib,siup_siujk,head_name,business_fields,' .
+            'director_name,director_position,director_phone,commissioner,year_established,' .
             'last_deed,province_id,regency_id,district_id,village,address,postal_code',
             'principal.province:id,code,name',
             'principal.regency:id,code,name',
@@ -32,7 +36,7 @@ class SubmissionController extends Controller
             'guarantorBranch:id,name',
             'guarantor.hostToHost:id,guarantor_id,guarantor_url_host,token',
             'product:id,name',
-            'guarantorToProductType:id,name,job_group,job_type',
+            'guarantorToProductType:id,product_type_id,name,job_group,job_type',
             'obligee:id,name,telephone,pic,no_ppk,province_id,regency_id,district_id,village,address,postal_code',
             'obligee.province:id,code,name',
             'obligee.regency:id,code,name',
@@ -54,12 +58,19 @@ class SubmissionController extends Controller
         $jobRegency = $submission->getRelation('regency');
         $jobDistrict = $submission->getRelation('district');
         $sourceOfFound = $submission->getRelation('sourceOfFund');
-        $hostToHost = $guarantor->getRelation('hostToHost');
-        $url = $hostToHost->guarantor_url_host;
-        $token = $hostToHost->token;
+        $products = Product::get(['id', 'name']);
+        $productTypes = ProductType::get(['id', 'name']);
+        $sourceOfFounds = SourceOfFund::get(['id', 'name']);
         $result = [
             'submission_id' => $submission->getAttribute('id'),
-            'principal' => (object) [
+            'resources' => [
+                "project_group" => JobGroup::getValues(),
+                "project_type" => JobType::getValues(),
+                "product" => $products->toArray(),
+                "product_type" => $productTypes->toArray(),
+                "source_of_fund" => $sourceOfFounds->toArray(),
+            ],
+            'principal' => [
                 'id' => $principal->getAttribute('id'),
                 'name' => $principal->getAttribute('name'),
                 'telephone' => $principal->getAttribute('telephone'),
@@ -69,85 +80,70 @@ class SubmissionController extends Controller
                 'siup_siujk' => $principal->getAttribute('siup_siujk'),
                 'head_name' => $principal->getAttribute('head_name'),
                 'business_fields' => $principal->getAttribute('business_fields'),
-                'director_name' => $principal->getAttribute('director_name'),
-                'director_position' => $principal->getAttribute('director_position'),
-                'director_phone' => $principal->getAttribute('director_phone'),
-                'commissioner' => $principal->getAttribute('commissioner'),
+                'director' => [
+                    'name' => $principal->getAttribute('director_name'),
+                    'position' => $principal->getAttribute('director_position'),
+                    'phone' => $principal->getAttribute('director_phone'),
+                    'commissioner' => $principal->getAttribute('commissioner'),
+                ],
                 'year_established' => $principal->getAttribute('year_established'),
                 'last_legality' => $principal->getAttribute('last_deed'),
-                'province' => (object) [
-                    'code' => $principal->province?->getAttribute('code'),
-                    'name' => $principal->province?->getAttribute('name'),
-                ],
-                'regency' => (object) [
-                    'code' => $principal->regency?->getAttribute('code'),
-                    'name' => $principal->regency?->getAttribute('name'),
-                ],
-                'district' => (object) [
-                    'code' => $principal->district?->getAttribute('code'),
-                    'name' => $principal->district?->getAttribute('name'),
-                ],
-                'village' => $principal->getAttribute('village'),
-                'address' => $principal->getAttribute('address'),
-                'postal_code' => $principal->getAttribute('postal_code'),
+                'location' => [
+                    'province' => $principal->province?->only(['code', 'name']),
+                    'regency' => $principal->regency?->only(['code', 'name']),
+                    'district' => $principal->district?->only(['code', 'name']),
+                    'village' => $principal->getAttribute('village'),
+                    'address' => $principal->getAttribute('address'),
+                    'postal_code' => $principal->getAttribute('postal_code'),
+                ]
             ],
-            'guarantee' => (object) [
+            'guarantee' => [
                 'no' => $submission->getAttribute('no_guarantee'),
                 'value' => $submission->getAttribute('guarantee_value'),
             ],
-            'contract' => (object) [
-                'value' => $submission->getAttribute('contract_value'),
-                'doc_name' => $submission->getAttribute('doc_name'),
-                'doc_number' => $submission->getAttribute('doc_number'),
-                'doc_date' => $submission->getAttribute('doc_date'),
+            'contract' => [
                 'blank' => $blank?->number,
-                'guarantor_name' => $guarantor->getAttribute('name'),
-                'guarantor_branch_name' => $guarantorBranch?->getAttribute('name'),
-                'product_name' => $product->getAttribute('name'),
-                'product_type_name' => $guarantorToProductType->getAttribute('name'),
-                'obligee' => (object) [
-                    'id' => $obligee->getAttribute('id'),
+                'value' => $submission->getAttribute('contract_value'),
+                'document' => [
+                    'name' => $submission->getAttribute('doc_name'),
+                    'number' => $submission->getAttribute('doc_number'),
+                    'date' => $submission->getAttribute('doc_date'),
+                ],
+                'guarantor' => [
+                    ...$guarantor->only(['id', 'code', 'name']),
+                    'branch' => $guarantorBranch?->only(['id', 'code', 'name']),
+                ],
+                'product' => $product->only(['id', 'name']),
+                'product_type' => [
+                    'id' => $guarantorToProductType->getAttribute('product_type_id'),
+                    'name' => $guarantorToProductType->getAttribute('name'),
+                ],
+                'obligee' => [
                     'name' => $obligee->getAttribute('name'),
                     'telephone' => $obligee->getAttribute('telephone'),
                     'pic' => $obligee->getAttribute('pic'),
                     'no_ppk' => $obligee->getAttribute('npwp'),
-                    'province' => (object) [
-                        'code' => $obligee->province?->getAttribute('code'),
-                        'name' => $obligee->province?->getAttribute('name'),
+                    'location' => [
+                        'province' => $obligee->province?->only(['code', 'name']),
+                        'regency' => $obligee->regency?->only(['code', 'name']),
+                        'district' => $obligee->district?->only(['code', 'name']),
+                        'village' => $obligee->getAttribute('village'),
+                        'address' => $obligee->getAttribute('address'),
+                        'postal_code' => $obligee->getAttribute('postal_code'),
                     ],
-                    'regency' => (object) [
-                        'code' => $obligee->regency?->getAttribute('code'),
-                        'name' => $obligee->regency?->getAttribute('name'),
-                    ],
-                    'district' => (object) [
-                        'code' => $obligee->district?->getAttribute('code'),
-                        'name' => $obligee->district?->getAttribute('name'),
-                    ],
-                    'village' => $obligee->getAttribute('village'),
-                    'address' => $obligee->getAttribute('address'),
-                    'postal_code' => $obligee->getAttribute('postal_code'),
                 ],
-                'project' => (object) [
+                'project' => [
                     'name' => $submission->getAttribute('job_name'),
                     'group' => $guarantorToProductType->getAttribute('job_group'),
                     'type' => $guarantorToProductType->getAttribute('job_type'),
                     'time_period' => $submission->getAttribute('time_period'),
                     'start_date' => $submission->getAttribute('start_date'),
                     'end_date' => $submission->getAttribute('end_date'),
-                    'source_of_fund' => $sourceOfFound->getAttribute('name'),
-                    'location' => (object) [
-                        'province' => (object) [
-                            'code' => $jobProvince->getAttribute('code'),
-                            'name' => $jobProvince->getAttribute('name'),
-                        ],
-                        'regency' => (object) [
-                            'code' => $jobRegency->getAttribute('code'),
-                            'name' => $jobRegency->getAttribute('name'),
-                        ],
-                        'district' => (object) [
-                            'code' => $jobDistrict->getAttribute('code'),
-                            'name' => $jobDistrict->getAttribute('name'),
-                        ],
+                    'source_of_fund' => $sourceOfFound->only(['id', 'name']),
+                    'location' => [
+                        'province' => $jobProvince->only(['code', 'name']),
+                        'regency' => $jobRegency->only(['code', 'name']),
+                        'district' => $jobDistrict->only(['code', 'name']),
                         'village' => $submission->getAttribute('job_location_village'),
                         'address' => $submission->getAttribute('job_location_address'),
                         'postal_code' => $submission->getAttribute('job_location_postal_code'),
@@ -155,11 +151,11 @@ class SubmissionController extends Controller
                 ],
             ],
             'output' => $submission->getRelation('submissionDocs')->map(function ($doc) {
-                return (object) [
+                return [
                     'name' => $doc->getAttribute('name'),
                     'value' => $doc->getAttribute('format_document'),
                 ];
-            }),
+            })->toArray(),
         ];
 
         return $this->responseSuccess('success', $result);
--- app/Http/Controllers/Submission/SubmissionController.php
+++ app/Http/Controllers/Submission/SubmissionController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Submission;
 
+use App\Enums\JobGroup;
+use App\Enums\JobType;
 use App\Enums\SubmissionStatus;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Submission\StoreRequest;
@@ -9,10 +11,13 @@ use App\Models\Document\DocumentFormat;
 use App\Models\Document\RequiredDoc;
 use App\Models\Guarantor\Blank;
 use App\Models\Guarantor\Guarantor;
+use App\Models\Product\Product;
+use App\Models\Product\ProductType;
 use App\Models\Profile\Profile;
 use App\Models\RelatedParties\Obligee;
 use App\Models\RelatedParties\Principal;
 use App\Models\Scoring\Scoring;
+use App\Models\Submission\SourceOfFund;
 use App\Models\Submission\Submission;
 use App\Models\Submission\SubmissionBlank;
 use App\Models\Submission\SubmissionDoc;
@@ -1912,82 +1917,102 @@ class SubmissionController extends Controller
         $jobRegency = $submission->getRelation('regency');
         $jobDistrict = $submission->getRelation('district');
         $sourceOfFound = $submission->getRelation('sourceOfFund');
-        $hostToHost = $guarantor->getRelation('hostToHost');
-        $url = $hostToHost->guarantor_url_host;
-        $token = $hostToHost->token;
+        $products = Product::get(['id', 'name']);
+        $productTypes = ProductType::get(['id', 'name']);
+        $sourceOfFounds = SourceOfFund::get(['id', 'name']);
         $result = [
-            'submission_id' => $submission->getAttribute('id') ?? '',
+            'submission_id' => $submission->getAttribute('id'),
+            'resources' => [
+                "project_group" => JobGroup::getValues(),
+                "project_type" => JobType::getValues(),
+                "product" => $products->toArray(),
+                "product_type" => $productTypes->toArray(),
+                "source_of_fund" => $sourceOfFounds->toArray(),
+            ],
             'principal' => [
-                'name' => $principal->getAttribute('name') ?? '',
-                'telephone' => $principal->getAttribute('telephone') ?? '',
-                'pic' => $principal->getAttribute('pic') ?? '',
-                'npwp' => $principal->getAttribute('npwp') ?? '',
-                'nib' => $principal->getAttribute('nib') ?? '',
-                'siup_siujk' => $principal->getAttribute('siup_siujk') ?? '',
-                'head_name' => $principal->getAttribute('head_name') ?? '',
-                'business_field' => $principal->getAttribute('business_fields') ?? '',
-                'director_name' => $principal->getAttribute('director_name') ?? '',
-                'director_position' => $principal->getAttribute('director_position') ?? '',
-                'director_phone' => $principal->getAttribute('director_phone') ?? '',
-                'commissioner' => $principal->getAttribute('commissioner') ?? '',
-                'year_established' => $principal->getAttribute('year_established') ?? '',
-                'last_legality' => $principal->getAttribute('last_deed') ?? '',
-                'province' => [
-                    'code' => $principal->province?->getAttribute('code') ?? '',
-                    'name' => $principal->province?->getAttribute('name') ?? '',
+                'id' => $principal->getAttribute('id'),
+                'name' => $principal->getAttribute('name'),
+                'telephone' => $principal->getAttribute('telephone'),
+                'pic' => $principal->getAttribute('pic'),
+                'npwp' => $principal->getAttribute('npwp'),
+                'nib' => $principal->getAttribute('nib'),
+                'siup_siujk' => $principal->getAttribute('siup_siujk'),
+                'head_name' => $principal->getAttribute('head_name'),
+                'business_fields' => $principal->getAttribute('business_fields'),
+                'director' => [
+                    'name' => $principal->getAttribute('director_name'),
+                    'position' => $principal->getAttribute('director_position'),
+                    'phone' => $principal->getAttribute('director_phone'),
+                    'commissioner' => $principal->getAttribute('commissioner'),
                 ],
-                'regency' => [
-                    'code' => $principal->regency?->getAttribute('code') ?? '',
-                    'name' => $principal->regency?->getAttribute('name') ?? '',
-                ],
-                'district' => [
-                    'code' => $principal->district?->getAttribute('code') ?? '',
-                    'name' => $principal->district?->getAttribute('name') ?? '',
-                ],
-                'village' => $principal->getAttribute('village') ?? '',
-                'address' => $principal->getAttribute('address') ?? '',
-                'postal_code' => $principal->getAttribute('postal_code') ?? '',
+                'year_established' => $principal->getAttribute('year_established'),
+                'last_legality' => $principal->getAttribute('last_deed'),
+                'location' => [
+                    'province' => $principal->province?->only(['code', 'name']),
+                    'regency' => $principal->regency?->only(['code', 'name']),
+                    'district' => $principal->district?->only(['code', 'name']),
+                    'village' => $principal->getAttribute('village'),
+                    'address' => $principal->getAttribute('address'),
+                    'postal_code' => $principal->getAttribute('postal_code'),
+                ]
             ],
             'guarantee' => [
-                'no' => $submission->getAttribute('no_guarantee') ?? '',
-                'value' => $submission->getAttribute('guarantee_value') ?? '',
+                'no' => $submission->getAttribute('no_guarantee'),
+                'value' => $submission->getAttribute('guarantee_value'),
             ],
             'contract' => [
-                'value' => $submission->getAttribute('contract_value') ?? '',
-                'doc_name' => $submission->getAttribute('doc_name') ?? '',
-                'doc_number' => $submission->getAttribute('doc_number') ?? '',
-                'doc_date' => $submission->getAttribute('doc_date') ?? '',
-                'blank' => $blank?->number ?? '',
-                'guarantor_name' => $guarantor->getAttribute('name') ?? '',
-                'guarantor_branch_name' => $guarantorBranch?->getAttribute('name') ?? '',
-                'product_name' => $product->getAttribute('name') ?? '',
-                'product_type_name' => $guarantorToProductType->getAttribute('name') ?? '',
+                'blank' => $blank?->number,
+                'value' => $submission->getAttribute('contract_value'),
+                'document' => [
+                    'name' => $submission->getAttribute('doc_name'),
+                    'number' => $submission->getAttribute('doc_number'),
+                    'date' => $submission->getAttribute('doc_date'),
+                ],
+                'guarantor' => [
+                    ...$guarantor->only(['id', 'code', 'name']),
+                    'branch' => $guarantorBranch?->only(['id', 'code', 'name']),
+                ],
+                'product' => $product->only(['id', 'name']),
+                'product_type' => [
+                    'id' => $guarantorToProductType->getAttribute('product_type_id'),
+                    'name' => $guarantorToProductType->getAttribute('name'),
+                ],
                 'obligee' => [
-                    'name' => $obligee->getAttribute('name') ?? '',
-                    'telephone' => $obligee->getAttribute('telephone') ?? '',
-                    'pic' => $obligee->getAttribute('pic') ?? '',
-                    'no_ppk' => $obligee->getAttribute('npwp') ?? '',
-                    'province' => [
-                        'code' => $obligee->province?->getAttribute('code') ?? '',
-                        'name' => $obligee->province?->getAttribute('name') ?? '',
+                    'name' => $obligee->getAttribute('name'),
+                    'telephone' => $obligee->getAttribute('telephone'),
+                    'pic' => $obligee->getAttribute('pic'),
+                    'no_ppk' => $obligee->getAttribute('npwp'),
+                    'location' => [
+                        'province' => $obligee->province?->only(['code', 'name']),
+                        'regency' => $obligee->regency?->only(['code', 'name']),
+                        'district' => $obligee->district?->only(['code', 'name']),
+                        'village' => $obligee->getAttribute('village'),
+                        'address' => $obligee->getAttribute('address'),
+                        'postal_code' => $obligee->getAttribute('postal_code'),
                     ],
-                    'regency' => [
-                        'code' => $obligee->regency?->getAttribute('code') ?? '',
-                        'name' => $obligee->regency?->getAttribute('name') ?? '',
+                ],
+                'project' => [
+                    'name' => $submission->getAttribute('job_name'),
+                    'group' => $guarantorToProductType->getAttribute('job_group'),
+                    'type' => $guarantorToProductType->getAttribute('job_type'),
+                    'time_period' => $submission->getAttribute('time_period'),
+                    'start_date' => $submission->getAttribute('start_date'),
+                    'end_date' => $submission->getAttribute('end_date'),
+                    'source_of_fund' => $sourceOfFound->only(['id', 'name']),
+                    'location' => [
+                        'province' => $jobProvince->only(['code', 'name']),
+                        'regency' => $jobRegency->only(['code', 'name']),
+                        'district' => $jobDistrict->only(['code', 'name']),
+                        'village' => $submission->getAttribute('job_location_village'),
+                        'address' => $submission->getAttribute('job_location_address'),
+                        'postal_code' => $submission->getAttribute('job_location_postal_code'),
                     ],
-                    'district' => [
-                        'code' => $obligee->district?->getAttribute('code') ?? '',
-                        'name' => $obligee->district?->getAttribute('name') ?? '',
-                    ],
-                    'village' => $obligee->getAttribute('village') ?? '',
-                    'address' => $obligee->getAttribute('address') ?? '',
-                    'postal_code' => $obligee->getAttribute('postal_code') ?? '',
                 ],
             ],
             'output' => $submission->getRelation('submissionDocs')->map(function ($doc) {
                 return [
-                    'name' => $doc->getAttribute('name') ?? '',
-                    'value' => $doc->getAttribute('format_document') ?? '',
+                    'name' => $doc->getAttribute('name'),
+                    'value' => $doc->getAttribute('format_document'),
                 ];
             })->toArray(),
         ];
