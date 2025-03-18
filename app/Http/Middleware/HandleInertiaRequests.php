5f953d9a18b3d439ffeecb2abc5ac943c4ce557e
Iman Nurjaman <imannj69@gmail.com>
2025-03-19 05:25:33 +0700

    update GuarantorController.php

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -34,17 +34,17 @@ class HandleInertiaRequests extends Middleware
     {
         return [
             ...parent::share($request),
-            'ziggy' => fn() => [
+            'ziggy' => fn () => [
                 ...(new Ziggy)->toArray(),
                 'location' => $request->url(),
             ],
-            'auth' => fn() => [
+            'auth' => fn () => [
                 'user' => $request->user()?->load('role'),
             ],
-            'roles' => fn() => (object)RoleEnum::getKeyValue(),
+            'roles' => fn () => (object) RoleEnum::getKeyValue(),
             'guarantor' => Guarantor::select(['id', 'name', 'picture'])->firstWhere('id', session('guarantor_id', config('guarantor.id'))),
-            'location' => fn() => $request->url(),
-            'flash_message' => fn() => [
+            'location' => fn () => $request->url(),
+            'flash_message' => fn () => [
                 'title' => $request->session()->get('title'),
                 'description' => $request->session()->get('description'),
                 'type' => $request->session()->get('type'),
--- routes/web/explore/explore.php
+++ routes/web/explore/explore.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\ExplorController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/explore', [ExplorController::class, 'index'])
-  ->name('explore.index');
+    ->name('explore.index');
