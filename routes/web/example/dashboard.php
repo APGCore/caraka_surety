a60bb766df58482c03c6356c38a16d6055d771f1
Iman Nurjaman <imannj69@gmail.com>
2025-03-26 06:48:00 +0700

    update test upload file

--- routes/web/example/dashboard.php
+++ routes/web/example/dashboard.php
@@ -7,8 +7,11 @@ Route::get('/example', [Controllers\ExampleController::class, 'index'])
   ->name('example.index');
 
 Route::get('/upload-testing', function () {
+  $filePath = 'uploads/test.txt';
+
   try {
-    Storage::disk('s3')->put('test.txt', 'Isi file dari Laravel');
+    Storage::disk('s3')->put($filePath, 'Laravel Upload Test');
+    echo "Upload berhasil ke path: " . $filePath;
     echo "File berhasil diupload ke S3";
   } catch (Exception $e) {
     echo "Error: " . $e->getMessage();
