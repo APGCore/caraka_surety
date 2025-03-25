76a2e80552ea1d7b76b65d53b37e5d13224b3167
Iman Nurjaman <imannj69@gmail.com>
2025-03-26 06:28:24 +0700

    update upload file

--- routes/web/example/dashboard.php
+++ routes/web/example/dashboard.php
@@ -4,4 +4,13 @@ use App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/example', [Controllers\ExampleController::class, 'index'])
-    ->name('example.index');
+  ->name('example.index');
+
+Route::get('/upload-testing', function () {
+  try {
+    Storage::disk('s3')->put('test.txt', 'Isi file dari Laravel');
+    echo "File berhasil diupload ke S3";
+  } catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+  }
+})->name('example.upload-testing');
