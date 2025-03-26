2e5293b04050212acea4b449af81a98ccb6492f3
Iman Nurjaman <imannj69@gmail.com>
2025-03-27 03:35:21 +0700

    add clear log activity command

---
+++ app/Console/Commands/clearActivity.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Spatie\Activitylog\Models\Activity;
+
+class clearActivity extends Command
+{
+  /**
+   * The name and signature of the console command.
+   *
+   * @var string
+   */
+  protected $signature = 'app:clear-activity';
+
+  /**
+   * The console command description.
+   *
+   * @var string
+   */
+  protected $description = 'Command description';
+
+  /**
+   * Execute the console command.
+   */
+  public function handle()
+  {
+    Activity::query()->delete();
+  }
+}
