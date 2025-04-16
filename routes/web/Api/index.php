6bb7821358bd3eab25ddf84d2632568bb81f3f28
Arief Rachman Hakim <ariefrachmanhakim1001@gmail.com>
2025-04-16 22:11:15 +0700

    feat: edit created docs

--- routes/web/Api/index.php
+++ routes/web/Api/index.php
@@ -2,8 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth'])->prefix('api')->group(function () {
-    foreach (glob(__DIR__.'/*.php') as $file) {
-        require_once $file;
-    }
+// Route::middleware(['auth'])->prefix('api')->group(function () {
+//     foreach (glob(__DIR__.'/*.php') as $file) {
+//         require_once $file;
+//     }
+// });
+
+
+Route::prefix('api')->group(function () {
+  foreach (glob(__DIR__ . '/*.php') as $file) {
+    require_once $file;
+  }
 });
