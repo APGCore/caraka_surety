66f3c61877ebc1b250c9e4b18860cc0d2a9fa946
Arief Rachman Hakim <ariefrachmanhakim1001@gmail.com>
2025-04-19 23:11:16 +0700

    feat: datatabel host to host

---
+++ routes/web/Api/host-to-host.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\HostToHostController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('host-to-host-management')
+  ->name('api.host-to-host-management.')
+  ->group(function () {
+
+    Route::controller(HostToHostController::class)
+      ->prefix('host-to-host')
+      ->name('host-to-host.')->group(function () {
+
+        // NAME: api.host-to-host-management.host-to-host.search-host-to-host
+        // PATH: /host-to-host-management/host-to-host/search-host-to-host
+        Route::get('search-host-to-host', 'apiSearch')->name('search-host-to-host');
+      });
+  });
