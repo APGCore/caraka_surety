0c33c96811edae9cc06843942c58f41b93cd0ba4
Iman Nurjaman <imannj69@gmail.com>
2025-04-21 22:54:49 +0700

    feat: add principal experience

---
+++ app/Http/Controllers/RelatedParties/PrincipalExperienceController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\RelatedParties;
+
+use App\Http\Controllers\Controller;
+
+class PrincipalExperienceController extends Controller
+{
+    //
+}
---
+++ app/Models/RelatedParties/PrincipalExperience.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models\RelatedParties;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Laravel\Scout\Searchable;
+
+class PrincipalExperience extends Model
+{
+    use HasFactory, Searchable, SoftDeletes;
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    public function principal(): BelongsTo
+    {
+        return $this->belongsTo(Principal::class);
+    }
+}
