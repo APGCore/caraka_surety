77b610e0e2763fd50f10617461405b0009866cb7
Iman Nurjaman <imannj69@gmail.com>
2025-04-22 23:21:32 +0700

    update submission resource

--- app/Http/Resources/Submission/SubmissionResource.php
+++ app/Http/Resources/Submission/SubmissionResource.php
@@ -24,51 +24,51 @@ class SubmissionResource extends JsonResource
             'end_date' => $this->resource->end_date ? Carbon::parse($this->resource->end_date)->format('d F Y') : null,
             'created_at' => $this->resource->created_at?->format('d F Y H:i:s') ?? null,
             'blank' => $this->resource->blank ?? $this->resource->blanks->first(),
-            'blanks' => $this->resource->relationLoaded('blanks', $this->resource->blanks),
-            'principal' => $this->resource->relationLoaded('principal', function () {
+            'blanks' => $this->whenLoaded('blanks', $this->resource->blanks),
+            'principal' => $this->whenLoaded('principal', function () {
                 return [
                     'id' => $this->resource->principal->id,
                     'name' => $this->resource->principal->name,
                 ];
             }),
-            'guarantor' => $this->resource->relationLoaded('guarantor', function () {
+            'guarantor' => $this->whenLoaded('guarantor', function () {
                 return [
                     'id' => $this->resource->guarantor->id,
                     'name' => $this->resource->guarantor->name,
                 ];
             }),
-            'guarantor_branch' => $this->resource->relationLoaded('guarantorBranch', function () {
+            'guarantor_branch' => $this->whenLoaded('guarantorBranch', function () {
                 return [
                     'id' => $this->resource->guarantorBranch->id,
                     'name' => $this->resource->guarantorBranch->name,
                 ];
             }),
-            'product' => $this->resource->relationLoaded('product', function () {
+            'product' => $this->whenLoaded('product', function () {
                 return [
                     'id' => $this->resource->product->id,
                     'name' => $this->resource->product->name,
                 ];
             }),
-            'product_type' => $this->resource->relationLoaded('guarantorToProductType', function () {
+            'product_type' => $this->whenLoaded('guarantorToProductType', function () {
                 return [
                     'id' => $this->resource->guarantorToProductType->id,
                     'name' => $this->resource->guarantorToProductType->name,
                 ];
             }),
-            'obligee' => $this->resource->relationLoaded('obligee', function () {
+            'obligee' => $this->whenLoaded('obligee', function () {
                 return [
                     'id' => $this->resource->obligee->id,
                     'name' => $this->resource->obligee->name,
                 ];
             }),
-            'staff' => $this->resource->relationLoaded('staff', function () {
+            'staff' => $this->whenLoaded('staff', function () {
                 return [
                     'id' => $this->resource->staff->id,
                     'name' => $this->resource->staff->name,
                     'office' => $this->resource->staff->office->name,
                 ];
             }),
-            'submission_before' => $this->resource->relationLoaded('submissionBefore', function () {
+            'submission_before' => $this->whenLoaded('submissionBefore', function () {
                 return [
                     'id' => $this->resource->submissionBefore->id,
                     'blank' => $this->resource->submissionBefore->blanks->select(['number'])->firstWhere('is_broken', false),
