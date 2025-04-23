bc0c646eb7fecafc5c81ef4c87cf275fad1dd782
waaizulfahmi <wandaaizulf@gmail.com>
2025-04-24 00:58:45 +0700

    add  export from fck

--- resources/js/components/documents/tiny-mce-editor.tsx
+++ resources/js/components/documents/tiny-mce-editor.tsx
@@ -17,8 +17,8 @@ const TinyMCEEditor: React.FC<TinyMCEEditorProps> = ({ id, initialContent, onCon
 
     const tinymceScript = document.createElement("script");
     // tinymceScript.src = "/js/tinymce/tinymce.min.js";
-    tinymceScript.src =
-      "https://cdn.tiny.cloud/1/u348l644l38woikj2xo5cmq1huk2850gmjq4yxim6m1ih6gt/tinymce/6/tinymce.min.js";
+    // tinymceScript.src =
+    //   "https://cdn.tiny.cloud/1/u348l644l38woikj2xo5cmq1huk2850gmjq4yxim6m1ih6gt/tinymce/6/tinymce.min.js";
 
     tinymceScript.async = true;
 
@@ -90,7 +90,7 @@ const TinyMCEEditor: React.FC<TinyMCEEditorProps> = ({ id, initialContent, onCon
         selector: `#${id}`,
         apiKey: "u348l644l38woikj2xo5cmq1huk2850gmjq4yxim6m1ih6gt",
         height: 500,
-        plugins: "exportpdf exportword",
+        plugins: "exportpdf exportword export",
         toolbar: "undo redo fontselect  | bold italic | alignleft aligncenter alignright alignjustify | code ",
         branding: false,
         promotion: false,
