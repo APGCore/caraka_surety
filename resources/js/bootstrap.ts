b6eaa42d3f2060b3d1e45c3d481d9a65a2e5caea
Arief Rachman Hakim <ariefrachmanhakim1001@gmail.com>
2025-04-26 00:15:06 +0700

    feat: config boostra p

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -1,9 +1,22 @@
+// import axios from 'axios';
+
+// window.axios = axios;
+
+// window.axios.defaults.withCredentials = true;
+
+// window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
 import axios from "axios";
+import Cookies from "js-cookie";
 
-window.axios = axios;
+axios.defaults.withCredentials = true;
+axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
-window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
-
-window.axios.defaults.withCredentials = true;
-
-window.axios.defaults.withXSRFToken = true;
+// Optional: Add this only if CSRF still not working
+axios.interceptors.request.use((config) => {
+  const token = Cookies.get("XSRF-TOKEN");
+  if (token) {
+    config.headers["X-XSRF-TOKEN"] = decodeURIComponent(token);
+  }
+  return config;
+});
