6e14471683dd7e09f0d84924d4d1026f38474612
Iman Nurjaman <imannj69@gmail.com>
2025-04-25 23:20:42 +0700

    Revert "update handle csrf token"
    This reverts commit a08f91655889a04dd3eb85cc21bd40f5de2facd9.

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -2,12 +2,4 @@ import axios from "axios";
 
 window.axios = axios;
 
-window.axios.defaults.withCredentials = true
-
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
-
-const token = document.head.querySelector('meta[name="csrf-token"]') as HTMLMetaElement;
-
-if (token) {
-  axios.defaults.headers.common["X-CSRF-TOKEN"] = token.content;
-}
