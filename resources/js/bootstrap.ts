3ec628bd4979c3d923d88b01901075e540f93067
Iman Nurjaman <imannj69@gmail.com>
2025-04-25 23:19:14 +0700

    rollback

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -2,4 +2,12 @@ import axios from "axios";
 
 window.axios = axios;
 
+window.axios.defaults.withCredentials = true
+
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+const token = document.head.querySelector('meta[name="csrf-token"]') as HTMLMetaElement;
+
+if (token) {
+  axios.defaults.headers.common["X-CSRF-TOKEN"] = token.content;
+}
