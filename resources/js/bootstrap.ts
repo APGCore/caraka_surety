0647ee699ba0241081fb819a4c1948a53fef07c4
Arief Rachman Hakim <ariefrachmanhakim1001@gmail.com>
2025-04-27 21:15:58 +0700

    feat: make default

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -1,22 +1,22 @@
-// import axios from 'axios';
-
-// window.axios = axios;
-
-// window.axios.defaults.withCredentials = true;
-
-// window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
 import axios from "axios";
-import Cookies from "js-cookie";
 
-axios.defaults.withCredentials = true;
-axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+window.axios = axios;
 
-// Optional: Add this only if CSRF still not working
-axios.interceptors.request.use((config) => {
-  const token = Cookies.get("XSRF-TOKEN");
-  if (token) {
-    config.headers["X-XSRF-TOKEN"] = decodeURIComponent(token);
-  }
-  return config;
-});
+window.axios.defaults.withCredentials = true;
+
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+// import axios from "axios";
+// import Cookies from "js-cookie";
+
+// axios.defaults.withCredentials = true;
+// axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+// // Optional: Add this only if CSRF still not working
+// axios.interceptors.request.use((config) => {
+//   const token = Cookies.get("XSRF-TOKEN");
+//   if (token) {
+//     config.headers["X-XSRF-TOKEN"] = decodeURIComponent(token);
+//   }
+//   return config;
+// });
