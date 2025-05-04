aaa79c0b6be4bda08997a5178c7ea090c3b89da4
iman nurjaman <imannj69@gmail.com>
2025-05-04 22:11:47 +0700

    update contract document

--- resources/js/pages/staff/submission-management/create/index.tsx
+++ resources/js/pages/staff/submission-management/create/index.tsx
@@ -1418,7 +1418,7 @@ const SubmissionCreatePage: SubmissionCreatePageProps = ({ guarantor, submission
                       </div>
                       <div className="grid gap-1 w-full">
                         <Label className="text-md">Selisih Waktu</Label>
-                        <Select value={data.submission.difference_time_period}
+                        <Select value={data.submission.difference_time_period?.toString()}
                           onValueChange={(value) => {
                             setData("submission", {
                               ...data.submission,
