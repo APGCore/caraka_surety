4da3d304891734e0ec4985e9cd40f5f1525881c2
iman nurjaman <imannj69@gmail.com>
2025-05-20 13:27:43 +0700

    update delete pengajuan

--- app/Http/Controllers/Submission/SubmissionController.php
+++ app/Http/Controllers/Submission/SubmissionController.php
@@ -1839,8 +1839,8 @@ class SubmissionController extends Controller
             $submission->submissionDocs()->delete();
             $submission->supportDocs()->delete();
             $submission->callback()->delete();
-            $submission->load('blank');
-            foreach ($submission->getRelation('blank') as $blank) {
+            $submission->load('blanks');
+            foreach ($submission->getRelation('blanks') as $blank) {
                 $blank->update([
                     'is_picked' => false,
                     'is_used' => false,
