d9988d82e2cc58ade4ba396270744214a719c994
Arief Rachman Hakim <ariefrachmanhakim1001@gmail.com>
2025-07-28 22:40:55 +0700

    feat: laporan produksi

--- routes/web/report-management/report.php
+++ routes/web/report-management/report.php
@@ -9,22 +9,22 @@ use Illuminate\Support\Facades\Route;
 use Maatwebsite\Excel\Facades\Excel;
 
 Route::controller(ReportController::class)
-    ->middleware(HandleRoleUsers::class.':'.implode(',', [RoleEnum::Admin->value]))
-    ->group(function () {
-        Route::prefix('production')
-            ->name('production.')->group(function () {
-                Route::get('/', 'productionReport')->name('index');
-            });
-        Route::prefix('blank-usage')
-            ->name('blank-usage.')->group(function () {
-                Route::get('/', 'blankUsage')->name('index');
-                // Route::get('/export', 'exportBlankUsage')->name('export');
-                Route::get('/export-blank-usage', function () {
-                    return Excel::download(new BlankUsageExport, 'blank_usage.xlsx');
-                })->name('export-unit');
+  ->middleware(HandleRoleUsers::class . ':' . implode(',', [RoleEnum::Admin->value]))
+  ->group(function () {
+    Route::prefix('production')
+      ->name('production.')->group(function () {
+        Route::get('/', 'productionReportV2')->name('index');
+      });
+    Route::prefix('blank-usage')
+      ->name('blank-usage.')->group(function () {
+        Route::get('/', 'blankUsage')->name('index');
+        // Route::get('/export', 'exportBlankUsage')->name('export');
+        Route::get('/export-blank-usage', function () {
+          return Excel::download(new BlankUsageExport, 'blank_usage.xlsx');
+        })->name('export-unit');
 
-                Route::get('/export-blank-usage-per-branch', function () {
-                    return Excel::download(new BlankUsageBranchExport, 'blank_usage_per_branch.xlsx');
-                })->name('export-branch');
-            });
-    });
+        Route::get('/export-blank-usage-per-branch', function () {
+          return Excel::download(new BlankUsageBranchExport, 'blank_usage_per_branch.xlsx');
+        })->name('export-branch');
+      });
+  });
