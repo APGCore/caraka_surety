1089de0d192e656229e51c42d183aaa45a6f1dcc
iman nurjaman <imannj69@gmail.com>
2025-08-13 15:56:53 +0700

    update

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -77,15 +77,9 @@ class InvoiceController extends Controller
 
         // Paginate the results
         $submissions = Submission::search($request->get('search'))
-            ->query(function ($query) use ($date, $officeSelected, $guarantorSelected, $productSelected, $guarantorToProductType) {
+            ->query(function ($query) use ($submissionIds) {
                 $query
-                    //->whereNotIn('status', [SubmissionStatus::REJECTED->value, SubmissionStatus::PROCESS->value])
-                    ->where('has_send_to_guarantor', true)
-                    ->when($date->isNotEmpty(), fn ($q) => $q->whereBetween('created_at', $date))
-                    ->when($officeSelected, fn ($q) => $q->whereHas('staff', fn ($q) => $q->where('profile_id', $officeSelected)))
-                    ->when($guarantorSelected, fn ($q) => $q->where('guarantor_id', $guarantorSelected))
-                    ->when($productSelected, fn ($q) => $q->where('product_id', $productSelected))
-                    ->when($guarantorToProductType, fn ($q) => $q->where('guarantor_to_product_type_id', $guarantorToProductType->id))
+                    ->whereIn('id', $submissionIds)
                     ->with([
                         'submissionBefore:id,blank_id',
                         'submissionBefore.blank',
