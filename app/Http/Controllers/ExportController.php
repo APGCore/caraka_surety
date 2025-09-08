5398d91ff76fb734b9e79c306a45067eed499efd
iman nurjaman <imannj69@gmail.com>
2025-09-08 10:15:43 +0700

    update

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -9,9 +9,9 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Models\Submission\Submission;
+use PhpOffice\PhpSpreadsheet\Writer\Pdf\Mpdf;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\StreamedResponse;
-use Illuminate\Support\Carbon;
 use Mpdf\Mpdf;
 use App\Traits\ReplaceDocumentFormat;
 use TCPDF;
@@ -85,79 +85,7 @@ class ExportController extends Controller
         }
 
         // Buat array data yang akan replace placeholder
-        $data = [
-            'principal_name' => $submission->principal->name ?? '',
-            'location' => $submission->principal->address ?? '',
-            'npwp' => $submission->principal->npwp ?? '',
-            'nib' => $submission->principal->nib ?? '',
-            'telephone' => $submission->principal->telephone ?? '',
-            'director_name' => $submission->principal->director_name ?? '',
-            'director_phone' => $submission->principal->director_phone ?? '',
-            'bussiness_field' => $submission->principal->bussiness_field ?? '',
-            'principal_commissioner' => $submission->principal->commissioner ?? '',
-            'pic' => $submission->principal->pic ?? '',
-            'director_position' => $submission->principal->director_position ?? '',
-            'principal_address' => "{$submission->principal->address}, {$submission->principal->district->name}, {$submission->principal->regency->name}, {$submission->principal->province->name}",
-            'est_deed' => $submission->principal->est_deed ?? '',
-            'last_deed' => $submission->principal->last_deed ?? '',
-            'get_susunan_pengurus' => $submission->get_administators_principal ?? '',
-            'get_exp' => $submission->get_exp ?? '',
-            'bank_name' => $submission->bankBranch->name ?? '',
-            'bank_address' => $submission->bankBranch?->address ?? '',
-            'obligee_name' => $submission->obligee->name ?? '',
-            'obligee_address' => $submission->obligee->address ?? '',
-            'source_of_fund' => $submission->source_of_fund->name ?? '',
-            'ppk_name' => $submission->obligee->pic ?? '',
-            'ppk_number' => $submission->obligee->no_ppk ?? '',
-            'principal_siup' => $submission->principal->siup_siujk ?? '',
-            'obligee_city' => $submission->obligee->district->name ?? '',
-            'obligee_location' => "{$submission->obligee->address}, {$submission->obligee->district->name}, {$submission->obligee->regency->name}, {$submission->obligee->province->name}",
-            'guarantor_name' => $submission->guarantor->name ?? '',
-            'guarantor_address' => $submission->guarantor_address ?? '',
-            'guarantor_pic' => $submission->guarantor_pic ?? '',
-            'guarantor_location' => "{$submission->guarantor->address}, {$submission->guarantor->district->name}, {$submission->guarantor->regency->name}, {$submission->guarantor->province->name}",
-            'guarantor_city' => $submission->guarantor_city ?? '',
-            'source_of_fund_name' => $submission->source_of_fund->name ?? '',
-            'contract_value' => $submission->contract_value ? number_format($submission->contract_value, 0, ',', '.') : '',
-            'guarantee_value' => $submission->guarantee_value ? number_format($submission->guarantee_value, 0, ',', '.') : '',
-            'guarantee_type' => $submission->guarantorToProductType->name ?? '',
-            'no_guarantee' => $submission->no_guarantee ?? '',
-            'time_period' => $submission->time_period ?? '',
-            'job_name' => $submission->job_name ?? '',
-            'job_location_village' => $submission->job_location_village ?? '',
-            'contract_doc_name' => $submission->contract_doc_name ?? '',
-            'contract_doc_number' => $submission->contract_doc_number ?? '',
-            'contract_doc_date' => $submission->contract_doc_date ?? '',
-            'start_date' => $submission->start_date ? Carbon::parse($submission->start_date)->translatedFormat('d F Y') : '',
-            'end_date' => $submission->end_date ? Carbon::parse($submission->end_date)->translatedFormat('d F Y') : '',
-            'guarantee_issue_date' => $submission->guarantee_issue_date ?? '',
-            'submission_date' => $submission->created_at?->translatedFormat('d F Y') ?? '',
-            'day' => $submission->day_name ?? '',
-            // 'character_score'      => $submission->analysis->character ?? '',
-            // 'capacity_score'       => $submission->analysis->capacity ?? '',
-            // 'capital_score'        => $submission->analysis->capital ?? '',
-            // 'collateral_score'     => $submission->analysis->collateral ?? '',
-            // 'condition_score'      => $submission->analysis->condition ?? '',
-            'total_score' => $submission->total_score ?? '',
-            'recommendation' => $submission->recommendation ?? '',
-            'notes' => $submission->notes ?? '',
-            'analyst_name' => $submission->analyst_name ?? '',
-            'manager_technique_name' => $submission->manager_technique_name ?? '',
-            'branch_manager' => $submission->principal->director_name ?? '',
-            'job_location' => "{$submission->job_location_village}, {$submission->district->name}, {$submission->regency->name}, {$submission->province->name}",
-            'job_group' => $submission->guarantorToProductType->job_group ?? '',
-            'no' => $submission->id ?? '',
-            'city' => $submission->regency->name ?? '',
-            'mail_number' => $submission->mail_number ?? '',
-            'mail_number_resume' => $submission->mail_number_resume ?? '',
-            'underlying' => ($submission->contract_doc_name.' '.$submission->contract_doc_number.' '.$submission->job_name) ?? '',
-            'product_name' => $submission->product->name ?? '',
-            'submission_support_docs' => $submission->submission_support_docs ?? '',
-            'terbilang' => $submission->terbilang ?? '',
-            'terbilang_hari' => $submission->terbilang_hari ?? '',
-            'bank_branch_name' => $submission->bankBranch?->name ?? '',
-
-        ];
+        $data = $this->convertSubmission($submission);
         // dd($submission);
         $html = $this->replaceDocumentFormat($documentFormat, $data);
 
@@ -221,74 +149,10 @@ class ExportController extends Controller
             abort(404, 'Konten HTML tidak tersedia.');
         }
 
-        $data = [
-            'PRINCIPAL_NAME' => $submission->principal->name ?? '',
-            'location' => $submission->principal->address ?? '',
-            'npwp' => $submission->principal->npwp ?? '',
-            'nib' => $submission->principal->nib ?? '',
-            'telephone' => $submission->principal->telephone ?? '',
-            'director_name' => $submission->principal->director_name ?? '',
-            'director_phone' => $submission->principal->director_phone ?? '',
-            'bussiness_field' => $submission->principal->bussiness_field ?? '',
-            'principal_commissioner' => $submission->principal->commissioner ?? '',
-            'pic' => $submission->principal->pic ?? '',
-            'director_position' => $submission->principal->director_position ?? '',
-            'principal_address' => "{$submission->principal->address}, {$submission->principal->district->name}, {$submission->principal->regency->name}, {$submission->principal->province->name}",
-            'est_deed' => $submission->principal->est_deed ?? '',
-            'last_deed' => $submission->principal->last_deed ?? '',
-            'get_susunan_pengurus' => $submission->get_administators_principal ?? '',
-            'get_exp' => $submission->get_exp ?? '',
-            'bank_name' => $submission->bank->name ?? '',
-            'bank_address' => $submission->bankBranch?->address ?? '',
-            'obligee_name' => $submission->obligee->name ?? '',
-            'obligee_address' => $submission->obligee->address ?? '',
-            'source_of_fund' => $submission->source_of_fund->name ?? '',
-            'ppk_name' => $submission->obligee->pic ?? '',
-            'ppk_number' => $submission->obligee->no_ppk ?? '',
-            'principal_siup' => $submission->principal->siup_siujk ?? '',
-            'obligee_city' => $submission->obligee->district->name ?? '',
-            'obligee_location' => "{$submission->obligee->address}, {$submission->obligee->district->name}, {$submission->obligee->regency->name}, {$submission->obligee->province->name}",
-            'guarantor_name' => $submission->guarantor->name ?? '',
-            'guarantor_address' => $submission->guarantor_address ?? '',
-            'guarantor_pic' => $submission->guarantor_pic ?? '',
-            'guarantor_location' => "{$submission->guarantor->address}, {$submission->guarantor->district->name}, {$submission->guarantor->regency->name}, {$submission->guarantor->province->name}",
-            'guarantor_city' => $submission->guarantor_city ?? '',
-            'source_of_fund_name' => $submission->source_of_fund->name ?? '',
-            'contract_value' => $submission->contract_value ? number_format($submission->contract_value, 0, ',', '.') : '',
-            'guarantee_value' => $submission->guarantee_value ? number_format($submission->guarantee_value, 0, ',', '.') : '',
-            'guarantee_type' => $submission->guarantorToProductType->name ?? '',
-            'no_guarantee' => $submission->no_guarantee ?? '',
-            'time_period' => $submission->time_period ?? '',
-            'job_name' => $submission->job_name ?? '',
-            'job_location_village' => $submission->job_location_village ?? '',
-            'contract_doc_name' => $submission->contract_doc_name ?? '',
-            'contract_doc_number' => $submission->contract_doc_number ?? '',
-            'contract_doc_date' => $submission->contract_doc_date ?? '',
-            'start_date' => $submission->start_date ?? '',
-            'end_date' => $submission->end_date ?? '',
-            'guarantee_issue_date' => $submission->guarantee_issue_date ?? '',
-            'submission_date' => $submission->created_at?->translatedFormat('d F Y') ?? '',
-            'day' => $submission->day_name ?? '',
-            'total_score' => $submission->total_score ?? '',
-            'recommendation' => $submission->recommendation ?? '',
-            'notes' => $submission->notes ?? '',
-            'analyst_name' => $submission->analyst_name ?? '',
-            'manager_technique_name' => $submission->manager_technique_name ?? '',
-            'branch_manager' => $submission->principal->director_name ?? '',
-            'job_location' => "{$submission->job_location_village}, {$submission->district->name}, {$submission->regency->name}, {$submission->province->name}",
-            'job_group' => $submission->guarantorToProductType->job_group ?? '',
-            'no' => $submission->id ?? '',
-            'city' => $submission->regency->name ?? '',
-            'mail_number' => $submission->mail_number ?? '',
-            'mail_number_resume' => $submission->mail_number_resume ?? '',
-            'underlying' => ($submission->contract_doc_name.' '.$submission->contract_doc_number.' '.$submission->job_name) ?? '',
-            'product_name' => $submission->product->name ?? '',
-            'submission_support_docs' => $submission->submission_support_docs ?? '',
-            'terbilang' => $submission->terbilang ?? '',
-            'terbilang_hari' => $submission->terbilang_hari ?? '',
-        ];
-
-        $html = $this->replacePlaceholders($html, $data);
+        // Buat array data yang akan replace placeholder
+        $data = $this->convertSubmission($submission);
+        // dd($submission);
+        $html = $this->replaceDocumentFormat($documentFormat, $data);
 
         $phpWord = new PhpWord;
         $section = $phpWord->addSection();
