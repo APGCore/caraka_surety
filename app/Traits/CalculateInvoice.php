2928ae9d1fc6b84a291ccf15c97c77cd7716bce2
azzmnrwebdev <azzmnrwebdev@gmail.com>
2025-10-01 09:22:11 +0700

    update perhitungan invoice

--- app/Traits/CalculateInvoice.php
+++ app/Traits/CalculateInvoice.php
@@ -2,167 +2,50 @@
 
 namespace App\Traits;
 
-use Carbon\Carbon;
 use App\Enums\SubmissionStatus;
-use Illuminate\Support\Collection;
 use App\Models\Submission\Submission;
+use Illuminate\Support\Collection;
 
 trait CalculateInvoice
 {
   public function calculateGuarantor(Submission $submission): Collection
   {
-    if ($submission->getAttribute('submission_before_id') !== null) {
-      $oldSubmission = Submission::find($submission->getAttribute('submission_before_id'));
+    $submission->loadMissing(['guarantor.guarantorRate']);
+    $timePeriode = (int) $submission->getAttribute('time_period');
+    $guaranteeValue = (float) $submission->getAttribute('guarantee_value');
+    $guarantor = $submission->getRelation('guarantor');
+    $guarantorRate = $guarantor?->getRelation('guarantorRate')
+      ->where('guarantor_to_product_type_id', $submission->getAttribute('guarantor_to_product_type_id'))
+      ->first();
+    $minimum = (float) $guarantorRate?->getAttribute('minimum_payment') ?? 0;
+    $rate = (float) ($guarantorRate?->getAttribute('pay_rate') ?? 0) / 100;
+    $adm = (float) $guarantorRate?->getAttribute('payment_administration') ?? 0;
+    $brokenRate = (float) $guarantorRate?->getAttribute('broken_rate') ?? 0;
+    $revisedRate = (float) $guarantorRate?->getAttribute('revised_rate') ?? 0;
+    $commission = (float) ($guarantorRate?->getAttribute('commission') ?? 0) / 100;
+    $pph = (float) ($guarantorRate?->getAttribute('pph') ?? 0) / 100;
 
-      if ($oldSubmission && $oldSubmission->getAttribute('send_to_guarantor_at')) {
-        $oldDate = Carbon::parse($oldSubmission->getAttribute('send_to_guarantor_at'));
-        $newDate = Carbon::now();
-
-        $oldPeriod = $this->getPeriod($oldDate);
-        $newPeriod = $this->getPeriod($newDate);
-
-        $isSamePeriod = $oldDate->month === $newDate->month
-          && $oldDate->year === $newDate->year
-          && $oldPeriod === $newPeriod;
-
-        if ($isSamePeriod) {
-          $submission->loadMissing(['guarantor.guarantorRate']);
-          $timePeriode = (int) $submission->getAttribute('time_period');
-          $guaranteeValue = (float) $submission->getAttribute('guarantee_value');
-          $guarantor = $submission->getRelation('guarantor');
-          $guarantorRate = $guarantor?->getRelation('guarantorRate')
-            ->where('guarantor_to_product_type_id', $submission->getAttribute('guarantor_to_product_type_id'))
-            ->first();
-          $minimum = (float) $guarantorRate?->getAttribute('minimum_payment') ?? 0;
-          $rate = (float) ($guarantorRate?->getAttribute('pay_rate') ?? 0) / 100;
-          $adm = (float) $guarantorRate?->getAttribute('payment_administration') ?? 0;
-          $brokenRate = (float) $guarantorRate?->getAttribute('broken_rate') ?? 0;
-          $revisedRate = (float) $guarantorRate?->getAttribute('revised_rate') ?? 0;
-          $commission = (float) ($guarantorRate?->getAttribute('commission') ?? 0) / 100;
-          $pph = (float) ($guarantorRate?->getAttribute('pph') ?? 0) / 100;
-
-          return $this->extractedCapitalRates($timePeriode, null, $guaranteeValue, $rate, $minimum, $adm, $commission, $pph, $brokenRate, $revisedRate);
-        } else {
-          // dd('beda periode');
-          return collect([]);
-        }
-      } else {
-        return collect([]);
-      }
-    } else {
-      $submission->loadMissing(['guarantor.guarantorRate']);
-      $timePeriode = (int) $submission->getAttribute('time_period');
-      $guaranteeValue = (float) $submission->getAttribute('guarantee_value');
-      $guarantor = $submission->getRelation('guarantor');
-      $guarantorRate = $guarantor?->getRelation('guarantorRate')
-        ->where('guarantor_to_product_type_id', $submission->getAttribute('guarantor_to_product_type_id'))
-        ->first();
-      $minimum = (float) $guarantorRate?->getAttribute('minimum_payment') ?? 0;
-      $rate = (float) ($guarantorRate?->getAttribute('pay_rate') ?? 0) / 100;
-      $adm = (float) $guarantorRate?->getAttribute('payment_administration') ?? 0;
-      $brokenRate = (float) $guarantorRate?->getAttribute('broken_rate') ?? 0;
-      $revisedRate = (float) $guarantorRate?->getAttribute('revised_rate') ?? 0;
-      $commission = (float) ($guarantorRate?->getAttribute('commission') ?? 0) / 100;
-      $pph = (float) ($guarantorRate?->getAttribute('pph') ?? 0) / 100;
-
-      return $this->extractedCapitalRates($timePeriode, $guaranteeValue, null, $rate, $minimum, $adm, $commission, $pph, $brokenRate, $revisedRate);
-    }
+    return $this->extractedCapitalRates($timePeriode, $guaranteeValue, $rate, $minimum, $adm, $commission, $pph, $brokenRate, $revisedRate);
   }
 
   public function calculateOffice(Submission $submission): Collection
   {
-    if ($submission->getAttribute('submission_before_id') !== null) {
-      $newSubmission = null;
-      $oldSubmission = null;
+    $submission->loadMissing(['office.profileRate']);
+    $timePeriode = (int) $submission->getAttribute('time_period');
+    $guaranteeValue = (float) $submission->getAttribute('guarantee_value');
+    $office = $submission->getRelation('office');
+    $profileRate = $office->getRelation('profileRate')
+      ->where('guarantor_id', $submission->getAttribute('guarantor_id'))
+      ->where('guarantor_to_product_type_id', $submission->getAttribute('guarantor_to_product_type_id'))
+      ->first();
 
-      if ($submission->getAttribute('submission_before_id') !== null) {
-        $newSubmission = $submission;
-        $oldSubmission = Submission::find($newSubmission->getAttribute('submission_before_id'));
-      } else {
-        $oldSubmission = $submission;
-        $newSubmission = Submission::where('submission_before_id', $oldSubmission->id)->first();
-      }
+    $minimum = (float) ($profileRate?->getAttribute('minimum_bill') ?? 0);
+    $rate = (float) ($profileRate?->getAttribute('selling_rate') ?? 0);
+    $adm = (float) ($profileRate?->getAttribute('sales_administration') ?? 0);
+    $brokenRate = (float) ($profileRate?->getAttribute('broken_rate') ?? 0);
+    $revisedRate = (float) ($profileRate?->getAttribute('revised_rate') ?? 0);
 
-      $oldDate = Carbon::parse($oldSubmission->getAttribute('send_to_guarantor_at'));
-      $newDate = Carbon::parse($newSubmission->getAttribute('send_to_guarantor_at'));
-
-      $oldPeriod = $this->getPeriod($oldDate);
-      $newPeriod = $this->getPeriod($newDate);
-
-      $isSamePeriod = $oldDate->month === $newDate->month
-        && $oldDate->year === $newDate->year
-        && $oldPeriod === $newPeriod;
-
-      if ($isSamePeriod) {
-        $newSubmission->loadMissing(['office.profileRate']);
-        $guaranteeValue = (float) $newSubmission->getAttribute('guarantee_value');
-        $office = $newSubmission->getRelation('office');
-        $timePeriode = (int) $newSubmission->getAttribute('time_period');
-        $profileRate = $office->getRelation('profileRate')
-          ->where('guarantor_id', $newSubmission->getAttribute('guarantor_id'))
-          ->where('guarantor_to_product_type_id', $newSubmission->getAttribute('guarantor_to_product_type_id'))
-          ->first();
-
-        $rate = (float) ($profileRate?->getAttribute('selling_rate') ?? 0);
-        $adm = (float) ($profileRate?->getAttribute('sales_administration') ?? 0);
-        $minimum = (float) ($profileRate?->getAttribute('minimum_bill') ?? 0);
-        $brokenRate = (float) ($profileRate?->getAttribute('broken_rate') ?? 0);
-        $revisedRate = (float) ($profileRate?->getAttribute('revised_rate') ?? 0);
-
-        return $this->extractedSellingRates(null, $guaranteeValue, $rate, $timePeriode, $adm, $minimum, $brokenRate, $revisedRate);
-      } else {
-        $newSubmission->loadMissing(['office.profileRate']);
-        $guaranteeValue1 = (float) $newSubmission->getAttribute('guarantee_value');
-        $guaranteeValue2 = (float) $oldSubmission->getAttribute('guarantee_value');
-        $office = $newSubmission->getRelation('office');
-        $timePeriode = (int) $newSubmission->getAttribute('time_period');
-        $profileRate = $office->getRelation('profileRate')
-          ->where('guarantor_id', $newSubmission->getAttribute('guarantor_id'))
-          ->where('guarantor_to_product_type_id', $newSubmission->getAttribute('guarantor_to_product_type_id'))
-          ->first();
-
-        $rate = (float) ($profileRate?->getAttribute('selling_rate') ?? 0);
-        $adm = (float) ($profileRate?->getAttribute('sales_administration') ?? 0);
-        $minimum = (float) ($profileRate?->getAttribute('minimum_bill') ?? 0);
-        $brokenRate = (float) ($profileRate?->getAttribute('broken_rate') ?? 0);
-        $revisedRate = (float) ($profileRate?->getAttribute('revised_rate') ?? 0);
-
-        return $this->extractedSellingRates($guaranteeValue2, $guaranteeValue1, $rate, $timePeriode, $adm, $minimum, $brokenRate, $revisedRate);
-      }
-    } else {
-      $newSubmission = Submission::where('submission_before_id', $submission->id)->first();
-
-      $oldDate = Carbon::parse($submission->getAttribute('send_to_guarantor_at'));
-      $newDate = Carbon::parse($newSubmission->getAttribute('send_to_guarantor_at'));
-
-      $oldPeriod = $this->getPeriod($oldDate);
-      $newPeriod = $this->getPeriod($newDate);
-
-      $isSamePeriod = $oldDate->month === $newDate->month
-        && $oldDate->year === $newDate->year
-        && $oldPeriod === $newPeriod;
-
-      $submission->loadMissing(['office.profileRate']);
-      $timePeriode = (int) $submission->getAttribute('time_period');
-      $guaranteeValue = (float) $submission->getAttribute('guarantee_value');
-      $office = $submission->getRelation('office');
-      $profileRate = $office->getRelation('profileRate')
-        ->where('guarantor_id', $submission->getAttribute('guarantor_id'))
-        ->where('guarantor_to_product_type_id', $submission->getAttribute('guarantor_to_product_type_id'))
-        ->first();
-
-      $minimum = (float) ($profileRate?->getAttribute('minimum_bill') ?? 0);
-      $rate = (float) ($profileRate?->getAttribute('selling_rate') ?? 0);
-      $adm = (float) ($profileRate?->getAttribute('sales_administration') ?? 0);
-      $brokenRate = (float) ($profileRate?->getAttribute('broken_rate') ?? 0);
-      $revisedRate = (float) ($profileRate?->getAttribute('revised_rate') ?? 0);
-
-      if ($isSamePeriod) {
-        return $this->extractedSellingRates(null, $guaranteeValue, $rate, $timePeriode, $adm, $minimum, $brokenRate, $revisedRate);
-      } else {
-        return $this->extractedSellingRates($guaranteeValue, null, $rate, $timePeriode, $adm, $minimum, $brokenRate, $revisedRate);
-      }
-    }
+    return $this->extractedSellingRates($guaranteeValue, $rate, $timePeriode, $adm, $minimum, $brokenRate, $revisedRate);
   }
 
   public function calculatePrincipal(Submission $submission): Collection
@@ -182,7 +65,7 @@ trait CalculateInvoice
     $brokenRate = (float) ($principalRate?->getAttribute('broken_rate') ?? 0);
     $revisedRate = (float) ($principalRate?->getAttribute('revised_rate') ?? 0);
 
-    return $this->extractedSellingRates($guaranteeValue, null, $rate, $timePeriode, $adm, $minimum, $brokenRate, $revisedRate);
+    return $this->extractedSellingRates($guaranteeValue, $rate, $timePeriode, $adm, $minimum, $brokenRate, $revisedRate);
   }
 
   public function calculateCapitalRates(Submission $submission): Collection
@@ -210,7 +93,7 @@ trait CalculateInvoice
       $commission = 0;
     }
 
-    return $this->extractedCapitalRates($timePeriode, $guaranteeValue, null, $rate, $minimum, $adm, $commission, $pph, $brokenRate, $revisedRate);
+    return $this->extractedCapitalRates($timePeriode, $guaranteeValue, $rate, $minimum, $adm, $commission, $pph, $brokenRate, $revisedRate);
   }
 
   public function calculateSellingRates(Submission $submission): Collection
@@ -241,65 +124,31 @@ trait CalculateInvoice
       $adm = $revisedRate;
     }
 
-    return $this->extractedSellingRates($guaranteeValue, null, $rate, $timePeriode, $adm, $minimum, $brokenRate, $revisedRate);
+    return $this->extractedSellingRates($guaranteeValue, $rate, $timePeriode, $adm, $minimum, $brokenRate, $revisedRate);
   }
 
-  private function extractedSellingRates(
-    float|null $oldGuaranteeValue,
-    float|null $newGuaranteeValue,
-    float|int|null $rate,
-    int|null $timePeriode,
-    float|null $adm,
-    float|int|null $minimum,
-    float|null $brokenRate,
-    float|null $revisedRate,
-  ): Collection {
-    if ($oldGuaranteeValue && $newGuaranteeValue) {
-      $oldSubService = $oldGuaranteeValue * ($rate / 100);
-      $newSubService = $newGuaranteeValue * ($rate / 100);
-
-      $oldServiceCharges = $timePeriode > 90 ? ($oldSubService * $timePeriode) / 90 : $oldSubService;
-      $newServiceCharges = $timePeriode > 90 ? ($newSubService * $timePeriode) / 90 : $newSubService;
-
-      $oldTotal = max(($oldServiceCharges + $adm), $minimum);
-      $newTotal = max(($newServiceCharges + $adm), $minimum);
-      $total = $newTotal - $oldTotal;
-
-      return collect([
-        'minimum' => $minimum,
-        'rate' => $rate,
-        'adm' => $adm,
-        'broken_rate' => $brokenRate,
-        'revised_rate' => $revisedRate,
-        'service_charges' => $oldServiceCharges,
-        'total' => $total,
-      ]);
-    } else {
-      $guaranteeValue = $newGuaranteeValue ?? $oldGuaranteeValue;
-
-      $subService = $guaranteeValue * ($rate / 100);
-      $serviceCharges = $timePeriode > 90 ? ($subService * $timePeriode) / 90 : $subService;
-      $total = $newGuaranteeValue ? $adm : max(($serviceCharges + $adm), $minimum);
-      $premi = max($serviceCharges, $minimum);
-
-      return collect([
-        'minimum' => $minimum,
-        'rate' => $rate,
-        'adm' => $adm,
-        'broken_rate' => $brokenRate,
-        'revised_rate' => $revisedRate,
-        'service_charges' => $serviceCharges,
-        'total' => $total,
-        'premi' => $premi,
-      ]);
-    }
-  }
-
-  private function extractedCapitalRates(int $timePeriode, ?float $guaranteeValueOld, ?float $guaranteeValueNew, float $rate, float|int $minimum, float|int $adm, float $commission, float $pph, float|int $brokenRate, float|int $revisedRate): Collection
+  private function extractedSellingRates(float $guaranteeValue, float|int $rate, int $timePeriode, float $adm, float|int $minimum, float $brokenRate, float $revisedRate): Collection
   {
-    $newGuaranteeValue = $guaranteeValueNew ?? $guaranteeValueOld;
+    $subService = $guaranteeValue * ($rate / 100);
+    $serviceCharges = $timePeriode > 90 ? ($subService * $timePeriode) / 90 : $subService;
+    $total = max(($serviceCharges + $adm), $minimum);
+    $premi = max($serviceCharges, $minimum);
 
-    $serviceCharge = (float) $timePeriode > 91 ? (($newGuaranteeValue * $rate * $timePeriode) / 91) : ($newGuaranteeValue * $rate);
+    return collect([
+      'minimum' => $minimum,
+      'rate' => $rate,
+      'adm' => $adm,
+      'broken_rate' => $brokenRate,
+      'revised_rate' => $revisedRate,
+      'service_charges' => $serviceCharges,
+      'total' => $total,
+      'premi' => $premi,
+    ]);
+  }
+
+  private function extractedCapitalRates(int $timePeriode, float $guaranteeValue, float $rate, float|int $minimum, float|int $adm, float $commission, float $pph, float|int $brokenRate, float|int $revisedRate): Collection
+  {
+    $serviceCharge = (float) $timePeriode > 91 ? (($guaranteeValue * $rate * $timePeriode) / 91) : ($guaranteeValue * $rate);
     $premi = max($serviceCharge, $minimum);
     $total = max(($adm + $serviceCharge), ($adm + $premi));
     $commissionResult = $commission * $premi;
@@ -325,17 +174,5 @@ trait CalculateInvoice
       'nett_commission' => $nettCommission,
       'nett_premi' => $nettPremi,
     ]);
-  }
-
-  private function getPeriod(Carbon $date): int
-  {
-    $day = (int) $date->day;
-
-    if ($day >= 1 && $day <= 10) {
-      return 1;
-    } elseif ($day >= 11 && $day <= 20) {
-      return 2;
-    }
-    return 3;
   }
 }
