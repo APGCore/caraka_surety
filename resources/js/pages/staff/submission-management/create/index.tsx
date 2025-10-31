399e62716411a542004e5172b9044415bdafdfd9
azzmnrwebdev <azzmnrwebdev@gmail.com>
2025-10-31 16:42:31 +0700

    ubah tanggal awal jaminan ke default

--- resources/js/pages/staff/submission-management/create/index.tsx
+++ resources/js/pages/staff/submission-management/create/index.tsx
@@ -1465,7 +1465,7 @@ const SubmissionCreatePage: SubmissionCreatePageProps = ({ guarantor, product, s
                         <CalendarPicker
                           dateFormat="YYYY-MM-DD"
                           disabled={{
-                            before: subDays(new Date(), 272),
+                            before: subDays(new Date(), 90),
                           }}
                           initialDate={
                             data?.submission?.start_date
