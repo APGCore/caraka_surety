a8a5a1292b3722330f044b4c2a6f49dbb8f6cb81
azzmnrwebdev <azzmnrwebdev@gmail.com>
2025-11-07 13:32:42 +0700

    feat: buka tanggal awal jaminan sampe bulan juli

--- resources/js/pages/staff/submission-management/create/index.tsx
+++ resources/js/pages/staff/submission-management/create/index.tsx
@@ -1465,7 +1465,7 @@ const SubmissionCreatePage: SubmissionCreatePageProps = ({ guarantor, product, s
                         <CalendarPicker
                           dateFormat="YYYY-MM-DD"
                           disabled={{
-                            before: subDays(new Date(), 90),
+                            before: subDays(new Date(), 120),
                           }}
                           initialDate={
                             data?.submission?.start_date
