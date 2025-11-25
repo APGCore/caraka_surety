1d055827d210ba13bbb6f55f939a47536fdd0596
iman nurjaman <imannj69@gmail.com>
2025-11-25 23:19:37 +0700

    update route callback 2

---
+++ app/Http/Requests/Api/Submission/CallbackExtRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Api\Submission;
+
+use App\Models\Submission\Submission;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CallbackExtRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'submission_id' => 'required|exists:'.Submission::class.',id',
+            'doc_url' => 'required|url',
+            'image' => 'required|base64',
+            'no_policy' => 'required|string',
+        ];
+    }
+}
