a670403c84863eed512df580e50bc3943eafb935
azzmnrwebdev <azzmnrwebdev@gmail.com>
2026-01-08 10:32:21 +0700

    feat: update tanggal awal jaminan sampai bulan agustus

--- resources/js/pages/staff/submission-management/create/index.tsx
+++ resources/js/pages/staff/submission-management/create/index.tsx
@@ -1659,7 +1659,7 @@ const SubmissionCreatePage: SubmissionCreatePageProps = ({ guarantor, product, s
                         <CalendarPicker
                           dateFormat="YYYY-MM-DD"
                           disabled={{
-                            before: subDays(new Date(), 90),
+                            before: subDays(new Date(), 160),
                           }}
                           initialDate={
                             data?.submission?.start_date
